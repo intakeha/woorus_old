6ef3b9821411cc91ed94c9907a653caf9d28b86e
Chonabot Intakeha <intakeha@gmail.com>
2011-03-14 00:57:07 +0000

    git-svn-id: https://woorus.com/svn/repos@24 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/forgotPassword.php
+++ actions/forgotPassword.php
@@ -5,15 +5,70 @@
 require('connect.php');
 
 //get from form
-//$f_email_address = strtolower(strip_tags($_POST['username']));
-$f_email_address = strtolower(strip_tags(testemail1@gmail.com));
+//$f_email_address = strtolower(strip_tags($_POST['email']));
+$f_email_address = strtolower(strip_tags(alisonclairemurphy@gmail.com));
 
-//create new password from randomly generated number, encrypt & put into password_token
-//lookup id associated with email
-//send email to user with link containing id & token
+//check if email is a valid email & get ID
+$returned_id = checkUser($f_email_address);
 
-//set 
-//change update time
+if ($returned_id != NULL)
+{
+	//connect
+	require('connect.php');
+	$connection = mysql_connect($db_host, $db_user, $db_pass) or die;
 
+	//create new token from randomly generated number, encrypt & put into password_token
+	$token = rand(23456789, 98765432); //randomly generated number
+	$query = " UPDATE `users` SET password_token = '".$token."' WHERE id= '".$returned_id."'";
+	mysql_select_db($db_name);
+	$result = mysql_query($query, $connection) or die ("Error");
+
+	//send email to user with link containing id & token
+	
+	
+	exit();
+}
+else
+{
+	exit();
+}
+
+
+
+
+
+
+function checkUser($email)
+{
+	require('connect.php');
+	//connect
+	$connection = mysql_connect($db_host, $db_user, $db_pass) or die;
+
+	//check if user exists
+	$query = "SELECT id, email_verified from `users` WHERE email_address = '".$email."' ";
+	mysql_select_db($db_name);
+	$result = mysql_query($query, $connection) or die ("Error");
+
+	// if row exists -> email is already registered
+	if (mysql_num_rows($result) == 1)
+	{
+		//check if user has activated via email
+		$row = mysql_fetch_assoc($result);
+		$activated = $row['email_verified'];
+		if ($activated == '0')
+		{
+			die("Your Account is not yet activated. Please check your email \n");
+		}
+		
+		$id = $row['id'];
+		print "success \n";
+		return $id;
+	}
+	else
+	{
+		print "user id does not exist \n";
+		return NULL;
+	}
+}
 
 ?>
