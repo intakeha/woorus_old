3e48701cf90ea22d7de610274e86c7ccabc62ac5
Chonabot Intakeha <intakeha@gmail.com>
2011-03-20 23:32:51 +0000

    Added javascript validation to homepage (authentication, forgot password and registration)
    git-svn-id: https://woorus.com/svn/repos@25 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

---
+++ js/woorus.js
@@ -0,0 +1,124 @@
+// Validate forms via jQuery
+$(document).ready(function(){
+	// Validate login form
+	$("#login_form").validate({
+		rules: {						// Adding validation rules for each input
+			email: "email",
+			password: "required"			
+		},
+		messages: {						// Customized error messages for each error
+			email: "Please enter a valid email address.",
+			password: "Please enter a password."	
+		},
+		groups: {						// Group form fields to get on error message
+			email: "email password"
+		},
+		errorLabelContainer: $("#auth_error")	// Assign error output div
+	});	
+	
+	// Validate login form
+	$("#recover_form").validate({
+		rules: {
+			email: "email"
+		},
+		messages: {
+			email: "Please enter a valid email address."
+		},
+		errorLabelContainer: $("#auth_error")
+	});	
+	
+	// Validate registration form
+	$("#registration_form").validate({
+		rules: {
+			first_name: {
+				required: true,
+				validname: true,
+				minlength: 2
+			},
+			last_name: {
+				required: true,
+				validname: true,
+				minlength: 2			
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			confirm_email: {
+				required: true,
+				equalTo: "#email"
+			},
+			password: {
+				required: true,
+				rangelength: [6,20]
+			},
+			gender: {
+				selectfield: true
+			},
+			birthday_month: {
+				selectfield: true
+			},
+			birthday_day: {
+				selectfield: true
+			},
+			birthday_year: {
+				selectfield: true
+			},
+			city: {
+				required: true
+			}
+		},
+		messages: {
+			first_name: {
+				required: "You must fill in all fields.",
+				validname: "First name contains invalid characters.",
+				minlength: "Please provide your real first name."
+			},
+			last_name: {
+				required: "You must fill in all fields.",
+				validname: "Last name contains invalid characters.",
+				minlength: "Please provide your last name."			
+			},
+			email: {
+				required: "You must fill in all fields.",
+				email: "Please enter a valid email address."
+			},
+			confirm_email: {
+				required: "You must fill in all fields.",
+				equalTo: "Your emails do not match."
+			},
+			password: {
+				required: "You must fill in all fields.",
+				rangelength: "Your password must be between 6 and 20 characters long."
+			},
+			gender: {
+				selectfield: "Please select your gender."
+			},
+			birthday_month: {
+				selectfield: "Please select birthday month." 
+			},
+			birthday_day: {
+				selectfield: "Please select birthday date."
+			},
+			birthday_year: {
+				selectfield: "Please select birthday year."
+			},
+			city: {
+				required: "You must fill in all fields."
+			}
+		},
+		groups: {						// Group form fields to get on error message
+			first_name: "first_name last_name email confirm_email password gender birthday_month birthday_day birthday_year city"
+		},
+		errorLabelContainer: $("#registration_error")
+	});	
+	
+});
+
+jQuery.validator.addMethod("validname", function(value, element) {
+	return this.optional(element) || /^[a-z-'\s]+$/i.test(value);
+}, "The name contains invalid characters.");
+
+jQuery.validator.addMethod("selectfield", function(value, element) {
+	if(element.value == -1){return false;} else return true;
+}, "Please select an option.");
