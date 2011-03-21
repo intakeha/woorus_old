86da1ca20ef20a8fb015bee92e80f9e28c86cf58
Chonabot Intakeha <intakeha@gmail.com>
2011-03-21 01:49:39 +0000

    updated to include a check for the account already being activated
    git-svn-id: https://woorus.com/svn/repos@26 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/activate.php
+++ actions/activate.php
@@ -4,10 +4,7 @@ require('connect.php');
 $id = $_GET['id'];
 $token = $_GET['token'];
 
-//for testing
-//$id = "12";
-//$token = "79003140";
-
+//check if id & token are not null
 if ($id&&$token)
 {
 	//connect
@@ -15,15 +12,28 @@ if ($id&&$token)
 	mysql_select_db($db_name);
 
 	//check if id & token combination exists
-	$query = "SELECT id from `users` WHERE id = '$id' AND email_token = '$token'";
+	$query = "SELECT id, email_verified from `users` WHERE id = '$id' AND email_token = '$token'";
 	$result = mysql_query($query, $connection) or die ("Error");
 
 	// if row exists -> id/token combination is correct
 	if (mysql_num_rows($result) == 1)
 	{
-		$activate_query = "UPDATE users SET email_verified= '1' WHERE id = '$id'";  
-		$activate_result = mysql_query($activate_query, $connection) or die ("Error");
-		die("Account is Activated");
+		// check to make sure new email is not already activated
+		$row = mysql_fetch_assoc($result);
+		$verified = $row['email_verified'];
+		if ($verified == 0)
+		{
+			//set email as verified & token to NULL
+			$activate_query = "UPDATE users SET email_verified= '1' , email_token = NULL WHERE id = '$id'";  
+			$activate_result = mysql_query($activate_query, $connection) or die ("Error");
+			die("Account is Activated");
+		
+		}
+		else 
+		{
+			die("Account has already been activated");
+		}
+		
 	}
 	else
 	{
