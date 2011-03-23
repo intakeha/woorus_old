9cbdb9620ba5c2ed2c64e09b43392f3bfa9f8558
Chonabot Intakeha <intakeha@gmail.com>
2011-03-23 04:27:35 +0000

    changed small errors in syntax
    git-svn-id: https://woorus.com/svn/repos@30 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/register.php
+++ actions/register.php
@@ -141,7 +141,7 @@ function validateEmail($email)
 	{
 		die("Please fill in your email address.");
 	}
-	$regexp = "/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/"
+	$regexp = "/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/";
 	elseif (!preg_match $regexp, $email))
 	{
 		die ("Please enter a valid email address.");
