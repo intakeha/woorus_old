80876fd71d35133dccddd1b86c9789b3826afdc9
Chonabot Intakeha <intakeha@gmail.com>
2011-03-26 23:41:53 +0000

    troubleshooting
    git-svn-id: https://woorus.com/svn/repos@45 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/register.php
+++ actions/register.php
@@ -47,7 +47,7 @@ mysql_select_db($db_name);
 
 //check if email is already in system
 $namecheck_query = "SELECT email_address from users WHERE email_address = '".$f_email_address."'";
-$namecheck_result = mysql_query($namecheck_query, $connection) or die ("Error");
+$namecheck_result = mysql_query($namecheck_query, $connection) or die ("Error 1");
 $namecheck_count = mysql_num_rows($namecheck_result);
 
 if ($namecheck_count != 0)
@@ -61,11 +61,11 @@ if ($namecheck_count != 0)
 $query = "INSERT INTO `users` (id, first_name, last_name, email_address, temp_email_address, password, password_token, gender, birthday, user_country_id, user_state_id, user_city_id, social_status, join_date, update_time, email_token, email_verified, temp_email_verified) VALUES 
 (NULL, '".$f_first_name."', '".$f_last_name."', '".$f_email_address."', NULL, '".$f_password."', NULL, '".$f_gender."', '".$f_birthday."', '".$f_user_country_id."', '".$f_user_state_id."', '".$f_user_city_id."', '".$social_status."', NOW(), NOW(), '".$token."', '".$email_verified."', '".$temp_email_verified."')";
 
-$result = mysql_query($query, $connection) or die ("Error");
+$result = mysql_query($query, $connection) or die ("Error 2");
 
 //re-lookup ID based on email
 $id_query = "SELECT id from users WHERE email_address = '".$f_email_address."'";
-$id_result = mysql_query($id, $connection) or die ("Error");
+$id_result = mysql_query($id, $connection) or die ("Error 3");
 $id_count = mysql_num_rows($id_result);
 if ($id_count != 0)
 {
