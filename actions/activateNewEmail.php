132855af1346c62d592a6b6c074456ac9548eeb3
Chonabot Intakeha <intakeha@gmail.com>
2011-03-27 23:36:42 +0000

    added visual email field
    git-svn-id: https://woorus.com/svn/repos@78 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/activateNewEmail.php
+++ actions/activateNewEmail.php
@@ -1,8 +1,9 @@
 <?php
 
-//need to update to deal with visual email, email
+//takes in "visual" email, stores it in the "visual" email field & then stores the lowercase, gmail-safe email in 
 
 require('connect.php');
+require('validations.php');
 
 //$id = $_GET['id'];
 //$token = $_GET['token'];
@@ -18,7 +19,7 @@ if ($id&&$token)
 	mysql_select_db($db_name);
 
 	//check if id & token combination exists
-	$query = "SELECT id, temp_email_address, temp_email_verified from `users` WHERE id = '$id' AND email_token = '$token'";
+	$query = "SELECT id, temp_email_address, temp_email_verified from `users` WHERE id = '".$id."' AND email_token = '".$token."' ";
 	$result = mysql_query($query, $connection) or die ("Error");
 	
 	// if row exists -> id/token combination is correct
@@ -26,12 +27,15 @@ if ($id&&$token)
 	{
 		// check to make sure new email is not already activated
 		$row = mysql_fetch_assoc($result);
-		$new_email = $row['temp_email_address']; 
+		$new_email_visual = $row['temp_email_address']; 
 		$activated = $row['temp_email_verified'];
+		$new_email = gmail_check(strtolower($new_email_visual));
+		
 		if ($activated == 0)
 		{
-			//put new email in email field, set temp email to null, set temp_email_verified to 1	
-			$activate_query = "UPDATE users SET temp_email_verified= '1', temp_email_address = 'NULL', email_address = '$new_email' WHERE id = '$id'";  
+			//put new email in email field, set temp email to null, set temp_email_verified to 1	'".$f_last_name."'
+			
+			$activate_query = "UPDATE users SET temp_email_verified= '1', temp_email_address = NULL, email_address = '".$new_email."',  visual_email_address = '"$new_email_visual"' WHERE id = '".$id."' ";  
 			$activate_result = mysql_query($activate_query, $connection) or die ("Error");
 			die("Account is Activated");
 		}
--- actions/register.php
+++ actions/register.php
@@ -54,8 +54,8 @@ if ($namecheck_count != 0)
 }
 
 //enter user into system
-$query = "INSERT INTO `users` (id, first_name, last_name, email_address, temp_email_address, password, password_token, gender, birthday, user_country_id, user_state_id, user_city_id, social_status, join_date, update_time, email_token, email_verified, temp_email_verified) VALUES 
-(NULL, '".$f_first_name."', '".$f_last_name."', '".$f_email_address."', NULL, '".$f_password."', NULL, '".$f_gender."', '".$f_birthday."', '".$f_user_country_id."', '".$f_user_state_id."', '".$f_user_city_id."', '".$social_status."', NOW(), NOW(), '".$token."', '".$email_verified."', '".$temp_email_verified."')";
+$query = "INSERT INTO `users` (id, first_name, last_name, email_address, visual_email_address, temp_email_address, password, password_token, gender, birthday, user_country_id, user_state_id, user_city_id, social_status, join_date, update_time, email_token, email_verified, temp_email_verified) VALUES 
+(NULL, '".$f_first_name."', '".$f_last_name."', '".$f_email_address."', '".$f_visual_email."', NULL, '".$f_password."', NULL, '".$f_gender."', '".$f_birthday."', '".$f_user_country_id."', '".$f_user_state_id."', '".$f_user_city_id."', '".$social_status."', NOW(), NOW(), '".$token."', '".$email_verified."', '".$temp_email_verified."')";
 
 $result = mysql_query($query, $connection) or die ("Error 2");
 
