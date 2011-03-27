cd7cb08b11a8b33ca943db2c2dfec5b389f0005d
Chonabot Intakeha <intakeha@gmail.com>
2011-03-27 19:53:02 +0000

    checking syntax
    git-svn-id: https://woorus.com/svn/repos@66 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/changeSettings.php
+++ actions/changeSettings.php
@@ -42,7 +42,7 @@ mysql_select_db($db_name);
 //update all fields (keep join_date, social status, token, active account, password new, id)
 //change update time  
 
-echo $f_first_name."\n".$f_last_name."\n".$f_temp_email_address."\n".$f_gender."\n".$id;
+echo $f_first_name."\n".$f_last_name."\n".$f_temp_email_address."\n".$f_gender."\n".$id."\n".$f_birthday."\n".$f_user_country_id."\n".$f_user_state_id."\n".$f_user_city_id;
 
 $query = "UPDATE users SET first_name = $f_first_name, last_name = $f_last_name, temp_email_address =  $f_temp_email_address, gender = $f_gender, birthday = $f_birthday, user_country_id = $f_user_country_id, user_state_id = $f_user_state_id, user_city_id = $f_user_city_id WHERE id = $id";
 
