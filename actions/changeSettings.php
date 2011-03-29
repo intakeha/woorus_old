ad6955d6f0bebe93d478d1eab9f442ced3c5056c
Chonabot Intakeha <intakeha@gmail.com>
2011-03-29 02:46:17 +0000

    added redirect
    git-svn-id: https://woorus.com/svn/repos@86 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/changeSettings.php
+++ actions/changeSettings.php
@@ -43,7 +43,6 @@ $f_mail_message = checkboxValidate($_POST['mail_message']);
 $f_mail_contact = checkboxValidate($_POST['mail_contact']);
 $f_mail_calls = checkboxValidate($_POST['mail_calls']);
 
-echo $f_mail_interest."\n".$f_mail_message."\n".$f_mail_contact."\n".$f_mail_calls;
 
 
 //if passes all checks for user entered data
@@ -55,13 +54,15 @@ mysql_select_db($db_name, $connection);
 //update all fields (keep join_date, social status, token, active account, password new, id)
 //change update time  
 
+//if user wants to change password (depends on logic for site)
+$f_password_old = md5($f_password_old);
+
 $query_users = "UPDATE `users` SET first_name = '".$f_first_name."', last_name = '".$f_last_name."', temp_email_address =  '".$f_temp_email_address."', gender = '".$f_gender."', birthday = '".$f_birthday."', user_country_id = '".$f_user_country_id."', user_state_id = '".$f_user_state_id."', user_city_id = '".$f_user_city_id."', update_time = NOW() WHERE id = '".$id."'";
 $result = mysql_query($query_users, $connection) or die ("Error 2");
 
 $query_settings = "UPDATE `settings` SET interest_notify =  '".$f_mail_interest."', message_notify =  '".$f_mail_message."', contact_notify =  '".$f_mail_contact."', missed_call_notify =  '".$f_mail_calls."' WHERE user_id = '".$id."' ";
 $result = mysql_query($query_settings, $connection) or die ("Error 2");
 
-//if user wants to change password (depends on logic for site)
-$f_password_old = md5($f_password_old);
+header( 'Location: ../canvas.php?page=settings') ;
 
 ?>
