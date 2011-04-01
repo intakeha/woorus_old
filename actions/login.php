54950287660ebd4663508b7f78eaa4449b3dc205
Chonabot Intakeha <intakeha@gmail.com>
2011-04-01 03:15:19 +0000

    added sql for updateLoginTime at login
    git-svn-id: https://woorus.com/svn/repos@92 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/login.php
+++ actions/login.php
@@ -19,6 +19,8 @@ if ($returned_id != NULL)
 	session_start();
 	$_SESSION['id'] = $returned_id;
 	$_SESSION['email'] = $f_email_address;
+	updateLoginTime($returned_id);
+	header( 'Location: ../canvas.php' ) ;
 	
 	exit();
 }
@@ -51,8 +53,6 @@ function authenticate($email, $pass)
 		}
 		
 		$id = $row['id'];
-		//print "success \n";
-		header( 'Location: ../canvas.php' ) ;
 		return $id;
 	}
 	else
@@ -62,5 +62,30 @@ function authenticate($email, $pass)
 	}
 }
 
+
+function updateLoginTime($id)
+{
+
+require('connect.php');
+$connection = mysql_connect($db_host, $db_user, $db_pass) or die;
+
+//check if user has logged in before
+$query_checkLogin = "SELECT id from `user_login` WHERE user_id = '".$id."'";
+$checkLogin_result = mysql_query($query_checkLogin, $connection) or die ("Error");
+$checkLogin_count = mysql_num_rows($checkLogin_result);
+
+	if ($checkLogin_count == 0) // user does not exist, do an insert
+	{
+
+		$query_login = "INSERT INTO `user_login` (id, user_id, update_time) VALUES (NULL,  '".$id."', NOW())";
+		$result = mysql_query($query_login, $connection) or die ("Error 2");
+
+	}
+	else //user does exist, can do an update
+	{
+		$query_login = "UPDATE `user_login` SET update_time = NOW() WHERE user_id = '".$id."'";
+		$result = mysql_query($query_login, $connection) or die ("Error 2");
+	}
+}
 
 ?>
