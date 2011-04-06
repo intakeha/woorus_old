b91343490b5cee2f1f4c8764e4912b6212e3a436
Chonabot Intakeha <intakeha@gmail.com>
2011-04-06 04:50:09 +0000

    changed redirect email, took out echo's for troubleshooting
    git-svn-id: https://woorus.com/svn/repos@112 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/savePassword.php
+++ actions/savePassword.php
@@ -13,13 +13,9 @@ $f_password_new = validatePassword($_POST['new_password']);
 $f_password_confirm = validatePassword($_POST['confirm_password']); 
 checkPassword($f_password_new, $f_password_confirm);
 
-echo $id."\n";
-echo $f_password_new. $f_password_confirm."\n";
-
 //encrypt password
 $f_password_new = md5($f_password_new);
 
-echo $f_password_new."\n";
 
 //open database connection
 $connection = mysql_connect($db_host, $db_user, $db_pass) or die ("Error 1");
@@ -29,10 +25,9 @@ mysql_select_db($db_name, $connection);
 $query_password = "UPDATE `users` SET password = '".$f_password_new."', update_time = NOW() WHERE id = '".$id."' ";
 $result = mysql_query($query_password, $connection) or die ("Password Update Error");
 
-//update last user login, in LoginHelperFunctions.php
+//update last user login, & take to main page.
 updateLoginTime($id);
-
-header( 'Location: ../canvas.php?page=settings') ;
+header( 'Location: ../canvas.php' ) ;
 
 ?>
 
