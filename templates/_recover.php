ceb6ad2036b788bc42581cbd20d067d001c236c0
Chonabot Intakeha <intakeha@gmail.com>
2011-04-06 04:35:32 +0000

    Added _recover.php
    git-svn-id: https://woorus.com/svn/repos@108 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

---
+++ templates/_recover.php
@@ -0,0 +1,13 @@
+<div id="forgot_password">
+    <form id="recover_form" action="../actions/savePassword.php" method="POST">
+	    <div>
+            <ul>
+            <li class="recover_title">Please enter a new password:</li>
+            <li><label>New Password</label><input class="text_form" id="new_password" type="password" name="new_password" maxlength="20"></li>
+            <li><label>Confirm Password</label><input class="text_form" type="password" name="confirm_password" maxlength="20"></li>
+            </ul>
+            <input id="save_password" class="buttons" type="submit" value="Save"><input class="buttons" id="cancel_button" type="button" value="Cancel" onclick="location.href='index.php'" /><br />
+        </div>
+        <div id="settings_error" class="error_text"></div>
+    </form>
+</div>
