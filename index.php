1a4aca660bed1e60e245a1ad582fc0077e022779
Chonabot Intakeha <intakeha@gmail.com>
2011-04-22 04:34:39 +0000

    Trying ajax javascript outside of woorus.js -- forgot to add $document.ready
    git-svn-id: https://woorus.com/svn/repos@215 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- index.php
+++ index.php
@@ -12,16 +12,18 @@
 	<script type="text/javascript" src="js/slides.min.jquery.js "></script>
    	<script type="text/javascript" src="js/woorus.js"></script>
     <script type="text/javascript">
-	$('#login_form').submit(function(){
-		$.post(
-			"actions/login.php",
-			$('#login_form').serialize(),
-			function(data){
-				$('#auth_error').text(data); 
-			}
-		);
-		return false;
-	 }); 
+	$(document).ready(function(){
+		$('#login_form').submit(function(){
+			$.post(
+				"actions/login.php",
+				$('#login_form').serialize(),
+				function(data){
+					$('#auth_error').text(data); 
+				}
+			);
+			return false;
+		 }); 
+	 });
 	</script>
     
 </head>
