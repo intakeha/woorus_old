b2d50acd7ee61a1640ece23e0a187bd4ca1778ed
Chonabot Intakeha <intakeha@gmail.com>
2011-04-23 02:34:55 +0000

    Added jquery.form.js
    git-svn-id: https://woorus.com/svn/repos@230 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 	<link href="css/woorus.css" rel="stylesheet" type="text/css">
 	<script type="text/javascript" src="js/jquery.min.js"></script>
    	<script type="text/javascript" src="js/jquery.validate.js"></script>
+	<script type="text/javascript" src="js/jquery.form.js "></script>
 	<script type="text/javascript" src="js/slides.min.jquery.js "></script>
    	<script type="text/javascript" src="js/woorus.js"></script>
-   	<script type="text/javascript" src="js/ajax.js"></script>
     
 </head>
 <body>
