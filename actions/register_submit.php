f3a2e578817e38c5787dc6ffd346bf151efef5c9
Chonabot Intakeha <intakeha@gmail.com>
2011-04-23 09:48:32 +0000

    Display recaptcha error
    git-svn-id: https://woorus.com/svn/repos@236 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/register_submit.php
+++ actions/register_submit.php
@@ -64,7 +64,7 @@ $resp = recaptcha_check_answer ($privatekey,
 
 if (!$resp->is_valid) {
 	// What happens when the CAPTCHA was entered incorrectly
-	die ("The reCAPTCHA wasn't entered correctly. Please try again foo.");
+	die ("The reCAPTCHA wasn't entered correctly. Please try again."."(reCAPTCHA said: " . $resp->error . ")");
 } else {
 	// Your code here to handle a successful verification
 
