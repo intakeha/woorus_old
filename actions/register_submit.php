455946b7b7222a3f8fa1044a2a74648ef8b55587
Chonabot Intakeha <intakeha@gmail.com>
2011-04-23 09:34:45 +0000

    Checking reCaptcha logic
    git-svn-id: https://woorus.com/svn/repos@235 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/register_submit.php
+++ actions/register_submit.php
@@ -64,7 +64,7 @@ $resp = recaptcha_check_answer ($privatekey,
 
 if (!$resp->is_valid) {
 	// What happens when the CAPTCHA was entered incorrectly
-	die ("The reCAPTCHA wasn't entered correctly. Please try again.");
+	die ("The reCAPTCHA wasn't entered correctly. Please try again foo.");
 } else {
 	// Your code here to handle a successful verification
 
