03d474c380ad852c4480d6423077f51ec79bc65e
Chonabot Intakeha <intakeha@gmail.com>
2011-04-25 04:05:38 +0000

    changed json string to trim dates & include email
    git-svn-id: https://woorus.com/svn/repos@253 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/lookupSettings.php
+++ actions/lookupSettings.php
@@ -11,7 +11,7 @@ $connection = mysql_connect($db_host, $db_user, $db_pass) or die;
 mysql_select_db($db_name);
 
 //lookup user data
-$lookup_query = "SELECT first_name, last_name, gender, birthday, user_city_id  FROM `users` WHERE id =  '".$_SESSION['id']."' ";
+$lookup_query = "SELECT first_name, last_name, gender, birthday, user_city_id, visual_email_address  FROM `users` WHERE id =  '".$_SESSION['id']."' ";
 $result = mysql_query($lookup_query, $connection) or die ("Error");
 
 //if we found a match
@@ -24,11 +24,15 @@ if (mysql_num_rows($result) == 1)
 	$last_name = $row['last_name'];
 	$gender = $row['gender'];
 	$user_city_id = $row['user_city_id'];
+	$email = $row['visual_email_address'];
 	
 	$birthday = $row['birthday'];
 	$birthday_arr = explode("-", $birthday); //convert birthday into array of year-month-day
+	$year =  $birthday_arr[0];
+	$month = trim($birthday_arr[1], "0");
+	$day = trim($birthday_arr[2], "0");
 	
-	$user_data = array('first_name'=>$first_name, 'last_name'=>$last_name, 'gender'=>$gender, 'birthday_month'=>$birthday_arr[1], 'birthday_day'=>$birthday_arr[2], 'birthday_year'=>$birthday_arr[0], 'user_city_id'=>$user_city_id);
+	$user_data = array('first_name'=>$first_name, 'last_name'=>$last_name, 'gender'=>$gender, 'birthday_month'=>$month, 'birthday_day'=>$day, 'birthday_year'=>$year, 'user_city_id'=>$user_city_id, 'email'=>$email);
 	
 	
 	$output = json_encode($user_data);
