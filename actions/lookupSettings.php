4d879a15b743b04b76d60805cd0839d410bc391b
Chonabot Intakeha <intakeha@gmail.com>
2011-04-25 01:25:04 +0000

    added json string portion
    git-svn-id: https://woorus.com/svn/repos@250 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/lookupSettings.php
+++ actions/lookupSettings.php
@@ -5,28 +5,40 @@ require('connect.php');
 //this is called when the user clicks on the "Settings page" to pre-populate the form
 
 session_start();
+
+//connect
 $connection = mysql_connect($db_host, $db_user, $db_pass) or die;
 mysql_select_db($db_name);
 
+//lookup user data
 $lookup_query = "SELECT first_name, last_name, gender, birthday, user_city_id  FROM `users` WHERE id =  '".$_SESSION['id']."' ";
 $result = mysql_query($lookup_query, $connection) or die ("Error");
+
+//if we found a match
 if (mysql_num_rows($result) == 1)
 {
 	$row = mysql_fetch_assoc($result);
 	
+	//fetch data
 	$first_name = $row['first_name'];
 	$last_name = $row['last_name'];
 	$gender = $row['gender'];
-	$birthday = $row['birthday'];
 	$user_city_id = $row['user_city_id'];
 	
-	//echo $first_name.$last_name.$gender.$birthday.$user_city_id."\n";
+	$birthday = $row['birthday'];
+	$birthday_arr = explode("-", $birthday); //convert birthday into array of year-month-day
 	
-	$user_data = array ('first_name'=>$first_name,'last_name'=>$last_name,'gender'=>$gender ,'birthday'=>$birthday,'user_city_id'=>$user_city_id);
-	json_encode($user_data);
-	//print_r($user_data);
+	$user_data = array('first_name'=>$first_name, 'last_name'=>$last_name, 'gender'=>$gender, 'birthday_month'=>$birthday_arr[1], 'birthday_day'=>$birthday_arr[2], 'birthday_year'=>$birthday_arr[0], 'user_city_id'=>$user_city_id);
+	
+	
+	$output = json_encode($user_data);
+	print($output);
 
 }
+
+
+
+
 
 
 ?>
