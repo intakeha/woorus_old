64a5e15339c2f6524281f6b7ba1da8b928eed222
Chonabot Intakeha <intakeha@gmail.com>
2011-04-27 15:39:55 +0000

    Create .php pages for all header menus
    git-svn-id: https://woorus.com/svn/repos@256 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

---
+++ templates/_mosaic.php
@@ -0,0 +1,8 @@
+<div id="mosaic">
+    <div class="pagination_mosaic"><a class="arrows pagination_left" href="#"></a></div>
+    <div id="tileSearch">
+    </div>
+    <div class="pagination_mosaic"><a class="arrows pagination_right" href="#"></a></div>
+    <div id="mosaicWall">
+    </div>
+</div>
