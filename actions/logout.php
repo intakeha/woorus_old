e0aaa78dd9590ef13d3ee1f1a550d197c87f5b5f
Chonabot Intakeha <intakeha@gmail.com>
2011-04-28 01:50:25 +0000

    changed logout.php to logout of facebook as well.
    git-svn-id: https://woorus.com/svn/repos@257 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/logout.php
+++ actions/logout.php
@@ -1,8 +1,25 @@
 <?php
 require('connect.php');
+require('facebook.php');
 
 session_start();
 session_destroy();
+
+
+// Need to logout if a facebook user
+$session = $facebook->getSession();
+$me = null;
+if ($session) {
+  try {
+	$me = $facebook->api('/me');
+    } catch (FacebookApiException $e) {
+    error_log($e);
+
+if ($me) {
+	$logoutUrl = $facebook->getLogoutUrl();
+} 
+
+header($logoutUrl);
 
 header( 'Location: ../') ;
 
