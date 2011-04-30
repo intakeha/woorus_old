d10042800261a35ae61b964285b9f5da954f141a
Chonabot Intakeha <intakeha@gmail.com>
2011-04-30 22:48:58 +0000

    Removed re-direct on success
    git-svn-id: https://woorus.com/svn/repos@331 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- js/woorus.js
+++ js/woorus.js
@@ -316,7 +316,6 @@ $(document).ready(function(){
 					}else{
 						$('#settings_error').hide();
 						$('#settings_success').html(data.message);
-						window.location.href = "canvas.php?page=settings";			
 					}
 				}
 			);
@@ -466,11 +465,10 @@ $(document).ready(function(){
 				$('#settings_form_c').serialize(),
 				function(data){
 					if (data.success == 0){
-						$('#settings_error').html(data.message); 
+						$('#settings_error').text(data.message); 
 					}else{
 						$('#settings_error').hide();
-						$('#settings_success').html(data.message);
-						window.location.href = "canvas.php?page=settings";			
+						$('#settings_success').text(data.message);		
 					}
 				}
 			);
