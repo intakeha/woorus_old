ea8f9c9556ac4808336985c13623807a55b188cc
Chonabot Intakeha <intakeha@gmail.com>
2011-05-01 22:47:37 +0000

    checking back end login
    git-svn-id: https://woorus.com/svn/repos@382 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/activateNewEmail.php
+++ actions/activateNewEmail.php
@@ -31,6 +31,7 @@ if ($id&&$token)
 		$new_email = get_standard_email($new_email_visual);
 		$activate_query = "UPDATE `users` SET temp_email_address = NULL, email_address = '".mysql_real_escape_string($new_email)."',  visual_email_address = '".mysql_real_escape_string($new_email_visual)."', email_token = NULL WHERE id = '".mysql_real_escape_string($id)."' ";
 		$activate_result = mysql_query($activate_query, $connection) or die ("Error 2");
+		die("about to call function".$id);
 		backendLogin($id);
 	}
 	else
