8a2d06c798bf572a2cb16d4f7667a7a950f82791
Chonabot Intakeha <intakeha@gmail.com>
2011-05-02 00:12:21 +0000

    testing
    git-svn-id: https://woorus.com/svn/repos@399 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/registerHelperFunctions.php
+++ actions/registerHelperFunctions.php
@@ -1,23 +1,23 @@
 <?php
 
-require_once('connect.php');
+
 
 function checkEmailInSystem($email)
 {
+	require('connect.php');
+	//open database connection
+	$connection = mysql_connect($db_host, $db_user, $db_pass) or die("unable to connect to db");
+	mysql_select_db($db_name);
 
-//open database connection
-$connection = mysql_connect($db_host, $db_user, $db_pass) or die("unable to connect to db");
-mysql_select_db($db_name);
+	$email_query = "SELECT email_address from `users` WHERE email_address = '".mysql_real_escape_string($email)."'";
+	$email_result = mysql_query($email_query, $connection) or die ("Error");
+	$email_count = mysql_num_rows($email_result);
 
-$email_query = "SELECT email_address from `users` WHERE email_address = '".mysql_real_escape_string($email)."'";
-$email_result = mysql_query($email_query, $connection) or die ("Error");
-$email_count = mysql_num_rows($email_result);
-
-	if ($email_count != 0)
-	{
-		$error_message = "This email address is already registered with Woorus.";	
-		sendToJS(0, $error_message);
-	}
+		if ($email_count != 0)
+		{
+			$error_message = "This email address is already registered with Woorus.";	
+			sendToJS(0, $error_message);
+		}
 }
 
 
