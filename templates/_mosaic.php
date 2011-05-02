445d600bd5771d0f09b539887f44796a2ab60dd0
Chonabot Intakeha <intakeha@gmail.com>
2011-05-02 15:40:30 +0000

    Updated CSS for mosaic wall and removed jquery.form.js
    git-svn-id: https://woorus.com/svn/repos@416 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- templates/_mosaic.php
+++ templates/_mosaic.php
@@ -2,8 +2,9 @@
     <div class="pagination_mosaic"><a class="arrows pagination_left" href="#"></a></div>
     <form id="tsearch_form" action="../actions/tileSearch.php" method="POST">
     <div id="tiles">
-    <input class="text_form" type="text" id="tile_name" name="tile_name" value="Type an interest..." maxlength="60">
+    <input type="text" id="tile_search_field" name="tile_name" value="Type an interest..." onfocus="if($(this).val()=='Type an interest...'){$(this).val('')};" onblur="if($(this).val()==''){$(this).val('Type an interest...')};" maxlength="60">
     <input class="buttons" id="tile_search" type="submit" name="tile_search" value="Search">
+    <div id="tile_legend"><a href="#"><span class="legend_squares"></span>my tiles</a> <a href="#"><span class="legend_squares"></span>sponsored tiles</a> <a href="#"><span class="legend_squares"></span>community tiles</a></div>
     <div id="tiles_bank">
     	<ul id="tile_display">
         	<li>BEP</li>
