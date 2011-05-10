e9bef634fa75709e8b9afc9f0c766748346560b0
Chonabot Intakeha <intakeha@gmail.com>
2011-05-10 04:43:02 +0000

    moved creation of empty rows from register to activate
    git-svn-id: https://woorus.com/svn/repos@432 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/activate.php
+++ actions/activate.php
@@ -29,6 +29,13 @@ if ($id&&$token)
 			$activate_query = "UPDATE users SET email_verified= '1' , email_token = NULL WHERE id = '".mysql_real_escape_string($id)."' ";  
 			$activate_result = mysql_query($activate_query, $connection) or die ("Error");
 			backendLogin($id);
+			
+			for ($tile_placement = 1; $tile_placement <= 36; $tile_placement++){
+		
+			$query_mosaic_wall = "INSERT into `mosaic_wall` (id, user_id, tile_placement, tile_id, interest_id, update_time) VALUES (NULL, '".$id."', '".$tile_placement."', 0 , 0, NOW()) ";
+			$result = mysql_query($query_mosaic_wall, $connection) or die ("Error 2");
+			}
+			
 			header('Location: ../canvas.php');
 			//use function where WE log them in (for acticate, save password, etc)
 		
