8942b4e92686ef2e0d43aa84438cdd0b869be446
Chonabot Intakeha <intakeha@gmail.com>
2011-05-18 04:13:37 +0000

    testing
    git-svn-id: https://woorus.com/svn/repos@492 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/imageFunctions.php
+++ actions/imageFunctions.php
@@ -9,8 +9,35 @@ function getWidth($image);
 ------------------------------------------------------------*/
 
 function convertImage($image) {
+	list($imagewidth, $imageheight, $imageType) = getimagesize($image);
+		$imageType = image_type_to_mime_type($imageType);
+		$newImageWidth = ceil($width * $scale);
+		$newImageHeight = ceil($height * $scale);
+		$newImage = imagecreatetruecolor($newImageWidth,$newImageHeight);
+		switch($imageType) 
+		{
+			case "image/gif":
+				$source=imagecreatefromgif($image); 
+				break;
+			case "image/pjpeg":
+				$source=imagecreatefromjpeg($image); 
+				break;
+			case "image/jpeg":
+				$source=imagecreatefromjpeg($image); 
+				break;
+			case "image/jpg":
+				$source=imagecreatefromjpeg($image); 
+				break;
+			case "image/png":
+				$source=imagecreatefrompng($image); 
+				break;
+			case "image/x-png":
+				$source=imagecreatefrompng($image); 
+				break;
+		}
+
 	$new_image_name  = substr($image, 0, strrpos($image, '.')).".jpg";
-	imagejpeg($image, $new_image_name, 90); 
+	imagejpeg($source, $new_image_name, 90); 
 	return $new_image_name;
 }
 
