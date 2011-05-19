4e06ed5c9a923c712df150ca0d7dc9659f356c90
Chonabot Intakeha <intakeha@gmail.com>
2011-05-19 02:26:39 +0000

    deleting file when convert to jpeg from gif or png
    git-svn-id: https://woorus.com/svn/repos@506 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/imageFunctions.php
+++ actions/imageFunctions.php
@@ -8,42 +8,6 @@ function getWidth($image);
 
 ------------------------------------------------------------*/
 
-//combined with the resize function
-/* 
-function convertImage($image) {
-	list($imagewidth, $imageheight, $imageType) = getimagesize($image);
-		$imageType = image_type_to_mime_type($imageType);
-		$newImageWidth = ceil($width * $scale);
-		$newImageHeight = ceil($height * $scale);
-		$newImage = imagecreatetruecolor($newImageWidth,$newImageHeight);
-		switch($imageType) 
-		{
-			case "image/gif":
-				$source=imagecreatefromgif($image);
-				break;
-			case "image/pjpeg":
-				$source=imagecreatefromjpeg($image); 
-				break;
-			case "image/jpeg":
-				$source=imagecreatefromjpeg($image); 
-				break;
-			case "image/jpg":
-				$source=imagecreatefromjpeg($image); 
-				break;
-			case "image/png":
-				$source=imagecreatefrompng($image); 
-				break;
-			case "image/x-png":
-				$source=imagecreatefrompng($image); 
-				break;
-		}
-
-	$new_image_name  = substr($image, 0, strrpos($image, '.')).".jpg";
-	imagejpeg($source, $new_image_name, 90); 
-	return $new_image_name;
-}
-*/
-
 function resizeImage($image,$width,$height,$scale) {
 	list($imagewidth, $imageheight, $imageType) = getimagesize($image);
 	$imageType = image_type_to_mime_type($imageType);
@@ -53,88 +17,43 @@ function resizeImage($image,$width,$height,$scale) {
 	switch($imageType) {
 		case "image/gif":
 			$source=imagecreatefromgif($image); 
+			$delete_old = 1; //bc changed file to jpeg
 			break;
 		case "image/pjpeg":
 			$source=imagecreatefromjpeg($image); 
+			$delete_old = 0; //already a jpeg
 			break;
 		case "image/jpeg":
 			$source=imagecreatefromjpeg($image); 
+			$delete_old = 0; //already a jpeg
 			break;
 		case "image/jpg":
 			$source=imagecreatefromjpeg($image); 
+			$delete_old = 0; //already a jpeg
 			break;
 		case "image/png":
 			$source=imagecreatefrompng($image); 
+			$delete_old = 1; //bc changed file to jpeg
 			break;
 		case "image/x-png":
 			$source=imagecreatefrompng($image); 
+			$delete_old = 1; //bc changed file to jpeg
 			break;
   	}
 	imagecopyresampled($newImage,$source,0,0,0,0,$newImageWidth,$newImageHeight,$width,$height);
 	
 	$new_image_name  = substr($image, 0, strrpos($image, '.')).".jpg";
 	imagejpeg($newImage, $new_image_name,90); 
+	
+	//if we created a new file to convert to jpeg need to delete the old one.
+	 if ($delete_old == 1){
+		unlink($image);
+	 }
 		
 	chmod($new_image_name, 0777);
 	return $new_image_name;
 }
 
-
-/*
-function resizeImage($image,$width,$height,$scale) {
-	list($imagewidth, $imageheight, $imageType) = getimagesize($image);
-	$imageType = image_type_to_mime_type($imageType);
-	$newImageWidth = ceil($width * $scale);
-	$newImageHeight = ceil($height * $scale);
-	$newImage = imagecreatetruecolor($newImageWidth,$newImageHeight);
-	switch($imageType) {
-		case "image/gif":
-			$source=imagecreatefromgif($image); 
-			break;
-		case "image/pjpeg":
-			$source=imagecreatefromjpeg($image); 
-			break;
-		case "image/jpeg":
-			$source=imagecreatefromjpeg($image); 
-			break;
-		case "image/jpg":
-			$source=imagecreatefromjpeg($image); 
-			break;
-		case "image/png":
-			$source=imagecreatefrompng($image); 
-			break;
-		case "image/x-png":
-			$source=imagecreatefrompng($image); 
-			break;
-  	}
-	imagecopyresampled($newImage,$source,0,0,0,0,$newImageWidth,$newImageHeight,$width,$height);
-	
-	switch($imageType) {
-		case "image/gif":
-	  		imagegif($newImage,$image);
-			break;
-		case "image/pjpeg":
-			imagejpeg($newImage,$image,90); 
-			break;
-		case "image/jpeg":
-			imagejpeg($newImage,$image,90); 
-			break;
-		case "image/jpg":
-	  		imagejpeg($newImage,$image,90); 
-			break;
-		case "image/png":
-			imagepng($newImage,$image);  
-			break;
-		case "image/x-png":
-			imagepng($newImage,$image);  
-			break;
-	}
-	
-	
-	chmod($image, 0777);
-	return $image;
-}
-*/
 
 //You do not need to alter these functions
 function resizeThumbnailImage($thumb_image_name, $image, $width, $height, $start_width, $start_height, $scale){
@@ -191,5 +110,41 @@ function getWidth($image) {
 	return $width;
 }
 
+
+//combined with the resize function
+/* 
+function convertImage($image) {
+	list($imagewidth, $imageheight, $imageType) = getimagesize($image);
+		$imageType = image_type_to_mime_type($imageType);
+		$newImageWidth = ceil($width * $scale);
+		$newImageHeight = ceil($height * $scale);
+		$newImage = imagecreatetruecolor($newImageWidth,$newImageHeight);
+		switch($imageType) 
+		{
+			case "image/gif":
+				$source=imagecreatefromgif($image);
+				break;
+			case "image/pjpeg":
+				$source=imagecreatefromjpeg($image); 
+				break;
+			case "image/jpeg":
+				$source=imagecreatefromjpeg($image); 
+				break;
+			case "image/jpg":
+				$source=imagecreatefromjpeg($image); 
+				break;
+			case "image/png":
+				$source=imagecreatefrompng($image); 
+				break;
+			case "image/x-png":
+				$source=imagecreatefrompng($image); 
+				break;
+		}
+
+	$new_image_name  = substr($image, 0, strrpos($image, '.')).".jpg";
+	imagejpeg($source, $new_image_name, 90); 
+	return $new_image_name;
+}
+*/
 
 ?>
