8a85360f2be03714cba1aa79a6779b0e3a9577b7
Chonabot Intakeha <intakeha@gmail.com>
2011-05-19 04:43:49 +0000

    moved mosaic wall functions to a mosaicWallHelperFunctions.php. created add & delete tile scripts
    git-svn-id: https://woorus.com/svn/repos@514 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

---
+++ actions/addTileToWall.php
@@ -0,0 +1,28 @@
+<?php
+
+require('connect.php');
+require('validations.php');
+require('mosaicWallHelperFunctions.php');
+
+session_start();
+$user_id = $_SESSION['id'];
+$tile_id = $_POST["tile_id"]; //need to validate this?
+
+//connect
+$connection = mysql_connect($db_host, $db_user, $db_pass) or die("unable to connect to db");
+mysql_select_db($db_name);
+
+//get interest id from the the tileID
+$interest_id = getInterestfromTile($tile_id, $connection); 
+
+//update user interests table (requires interest id & tile id)
+updateUserInterestTable($user_id, $interest_id, $tile_id, $connection); //add this as an interest of the user, its *new* for them
+
+//lookup tile placment
+$tile_placement = getTilePlacement($user_id, $connection);
+
+//add to user's mosaic wall
+updateMosaicWallTable($user_id, $interest_id, $tile_id, $tile_placement, $connection);
+
+
+?>
--- actions/crop.php
+++ actions/crop.php
@@ -2,6 +2,7 @@
 require('imageFunctions.php');
 require('connect.php');
 require('validations.php');
+require('mosaicWallHelperFunctions.php');
 
 $thumb_width = "75";		// Width of thumbnail image
 $thumb_height = "75";		// Height of thumbnail image
@@ -83,76 +84,5 @@ $success_message = "Your new tile has been added to your wall.";
 $messageToSend = array('success' => 1, 'message'=>$success_message, 'filename'=>$picture_name, 'tile_id'=>$tile_id, 'tag'=>$tile_name);
 $output = json_encode($messageToSend);
 die($output);
-
-
-function lookupInterestID($interest, $connection)
-{
-	$interest_query = "SELECT id from `interests` WHERE interest_name = '".mysql_real_escape_string($interest)."' ";
-	$interest_result = mysql_query($interest_query, $connection) or die ("Error");
-	if (mysql_num_rows($interest_result) > 0)
-	{
-		$row = mysql_fetch_assoc($interest_result);
-		$interest_id = $row['id'];
-		return $interest_id;
-	}else
-	{
-		return NULL;
-	}
-}
-
-function updateUserInterestTable($user_id, $interest_id, $tile_id, $connection)
-{
-	$query_add_interest = "INSERT INTO `user_interests` (id, user_id, interest_id, tile_id, update_time) VALUES (NULL, '". mysql_real_escape_string($user_id)."' , '".mysql_real_escape_string($interest_id)."', '".mysql_real_escape_string($tile_id)."', NOW() )";
-	$result = mysql_query($query_add_interest, $connection) or die ("Error 7");
-}
-
-function updateTileTable($user_id, $interest_id, $picture_name, $connection)
-{
-	$query_update_tile = "INSERT INTO `tiles` (id, interest_id, tile_filename, update_time, picture_flagged, user_id, facebook_id) VALUES (NULL, '".mysql_real_escape_string($interest_id)."', '".mysql_real_escape_string($picture_name)."', NOW(), 0 ,'".mysql_real_escape_string($user_id)."', 0)";
-	$result = mysql_query($query_update_tile, $connection) or die ("Error 8");		
-}
-
-function updateMosaicWallTable($user_id, $interest_id, $tile_id, $tile_placement, $connection)
-{
-	$mosaic_wall_query = "UPDATE `mosaic_wall` SET tile_id = '".$tile_id."', interest_id = '".$interest_id."', update_time = NOW() WHERE user_id = '".$user_id."' AND tile_placement = '".$tile_placement."' ";
-	$mosaic_wall_result = mysql_query($mosaic_wall_query, $connection) or die ("Error 10");
-}
-
-
-function lookupTileID($picture_name, $connection)
-{
-	$tile_id_query = "SELECT id from `tiles` WHERE  tile_filename = '".mysql_real_escape_string($picture_name)."' ";
-	$tile_id_result = mysql_query($tile_id_query, $connection) or die ("Error 9");
-	$tile_id_count = mysql_num_rows($tile_id_result);
-	if ($tile_id_count != 0)
-	{
-		//get id
-		$row = mysql_fetch_assoc($tile_id_result);
-		$retrieved_tile_id = $row['id']; 
-		return $retrieved_tile_id;
-	}else
-	{
-		$error_message = "Error uploading tile.";
-		sendToJS(0, $error_message);
-	}
-}
-
-function getTilePlacement($user_id, $connection){
-
-	$tile_placement_query =  "SELECT min(tile_placement) from `mosaic_wall` where tile_id = 0 AND interest_id = 0 AND user_id = '".$user_id."' ";
-	$tile_placement_result = mysql_query($tile_placement_query, $connection) or die ("Error");
-	$tile_placement_count = mysql_num_rows($tile_placement_result); //necessary?
-	$row = mysql_fetch_assoc($tile_placement_result); //min function means that a row will always be returned
-	$tile_id = $row['min(tile_placement)']; 
-	
-	if ($tile_id == NULL)
-	{
-		$error_message = "Your wall is full. Please remove some tiles to add new tiles.";
-		sendToJS(0, $error_message);
-	}else
-	{
-		return $tile_id;
-	}
-}
 
 ?>
