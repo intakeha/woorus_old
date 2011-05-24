4cfc0c8743d25ae37aa433ab8a53b51130ea2008
Chonabot Intakeha <intakeha@gmail.com>
2011-05-24 04:35:58 +0000

    commented out testing code
    git-svn-id: https://woorus.com/svn/repos@557 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/moveTileOnWall.php
+++ actions/moveTileOnWall.php
@@ -10,12 +10,12 @@ This is for when a user deletes a tile from their wall.
 session_start();
 $user_id = $_SESSION['id'];
 
-//$tile_array = json_decode($_POST["tile_array"]); //need to validate this?
+$tile_array = json_decode($_POST["tile_array"], true); //need to validate this?
 
-$tile_array = json_decode('{"1":{"tile_id":"252","interest_id":"293"},"2":{"tile_id":"253","interest_id":"294"},"3":{"tile_id":"254","interest_id":"295"},"4":{"tile_id":"255","interest_id":"296"}}', true);
+//$tile_array = json_decode('{"1":{"tile_id":"252","interest_id":"293"},"2":{"tile_id":"253","interest_id":"294"},"3":{"tile_id":"254","interest_id":"295"},"4":{"tile_id":"255","interest_id":"296"}}', true);
 
 //connect
-$connection = mysql_connect($db_host, $db_user, $db_pass) or die("unable to connect to db");
+$connection = mysql_connect($db_host, $db_user, $db_pass) or die("Unable to connect to db.");
 mysql_select_db($db_name);
 
 $tile_placement = 1;
@@ -25,13 +25,13 @@ foreach ($tile_array as $value){
 	$interest_id = $value["interest_id"];
 
 	echo "tile_id is: ".$tile_id ." interest_id is: ".$interest_id." tile_placement is: ".$tile_placement;
-	//updateMosaicWallTable($user_id, $interest_id , $tile_id , $tile_placement, $connection);
+	updateMosaicWallTable($user_id, $interest_id , $tile_id , $tile_placement, $connection);
 	$tile_placement++;
 }
 
 //zero out the last one (if it was a delete)
 if ($tile_placement <= 36){
-	//updateMosaicWallTable($user_id, 0 , 0 , $tile_placement, $connection);
+	updateMosaicWallTable($user_id, 0 , 0 , $tile_placement, $connection);
 }
 
 unset($tile_placement);
