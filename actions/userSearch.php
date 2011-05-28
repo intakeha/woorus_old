7dab44a098c4e105b8370e2bbef1859e571921d1
Chonabot Intakeha <intakeha@gmail.com>
2011-05-28 22:06:04 +0000

    testing
    git-svn-id: https://woorus.com/svn/repos@595 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/userSearch.php
+++ actions/userSearch.php
@@ -18,7 +18,7 @@ mysql_select_db($db_name);
 
 
 //look for interest based on ID
-$interest_query = "SELECT id, interest_name FROM `interests` WHERE interest_name =  '".$interest_search."'  ";
+$interest_query = "SELECT id, interest_name FROM `interests` WHERE interest_name =  '".$user_search."'  ";
 $interest_result = mysql_query($interest_query, $connection) or die ("Error");
 if (mysql_num_rows($interest_result) > 0)
 {
