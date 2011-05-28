9bac2b00271c4a14cb1dd534e3c86e8be6e99cbe
Chonabot Intakeha <intakeha@gmail.com>
2011-05-28 22:06:54 +0000

    testing
    git-svn-id: https://woorus.com/svn/repos@596 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/userSearch.php
+++ actions/userSearch.php
@@ -16,10 +16,9 @@ $user_search = "penguins"; //need to validate this!!!
 $connection = mysql_connect($db_host, $db_user, $db_pass) or die;
 mysql_select_db($db_name);
 
-
 //look for interest based on ID
 $interest_query = "SELECT id, interest_name FROM `interests` WHERE interest_name =  '".$user_search."'  ";
-$interest_result = mysql_query($interest_query, $connection) or die ("Error");
+$interest_result = mysql_query($interest_query, $connection) or die ("Error 1");
 if (mysql_num_rows($interest_result) > 0)
 {
 	$row = mysql_fetch_assoc($interest_result);
@@ -35,7 +34,7 @@ else
 
 //Get users associated with the interest id retreived above (need to pull the interest ID and tile_id as well	
 $mosaic_query =  "SELECT user_id, interest_id, tile_id FROM `mosaic_wall` WHERE interest_id =  '".$interest_id."' AND interest_active = 1";
-$mosaic_result = mysql_query($mosaic_query, $connection) or die ("Error");
+$mosaic_result = mysql_query($mosaic_query, $connection) or die ("Error 2");
 if (mysql_num_rows($mosaic_result) == 0) //we found the interest, but its not on anyones wall
 {
 	$error_message = "We found no matches for your interest. Please search by a new interest or meet someone in the lounge. 2";
@@ -54,7 +53,7 @@ if (mysql_num_rows($mosaic_result) == 0) //we found the interest, but its not on
 		
 		//get user info based on user ID (need to change this to look at online status!!)
 		$user_query = "SELECT id, first_name, social_status FROM `users` WHERE id =  '".$user_search_id."' ";
-		$user_query_result = mysql_query($user_query, $connection) or die ("Error 9");
+		$user_query_result = mysql_query($user_query, $connection) or die ("Error 3");
 		if (mysql_num_rows($user_query_result) > 0)
 		{
 			$row = mysql_fetch_assoc($user_query_result);
