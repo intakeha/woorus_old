f75d4af1046442961252989f1fb1f7e176af469b
Chonabot Intakeha <intakeha@gmail.com>
2011-05-30 21:20:06 +0000

    changed incoming format for tile array
    git-svn-id: https://woorus.com/svn/repos@604 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/moveTileOnWall.php
+++ actions/moveTileOnWall.php
@@ -10,9 +10,7 @@ This is for when a user deletes a tile from their wall.
 session_start();
 $user_id = $_SESSION['id'];
 
-$tile_array = json_decode($_POST["tile_array"], true); //need to validate this?
-
-//$tile_array = json_decode('{"1":{"tile_id":"252","interest_id":"293"},"2":{"tile_id":"253","interest_id":"294"},"3":{"tile_id":"254","interest_id":"295"},"4":{"tile_id":"255","interest_id":"296"}}', true);
+$tile_array = explode(",", $_POST["tile_array"]); //need to validate this?
 
 //connect
 $connection = mysql_connect($db_host, $db_user, $db_pass) or die("Unable to connect to db.");
@@ -21,8 +19,8 @@ mysql_select_db($db_name);
 $tile_placement = 1;
 
 foreach ($tile_array as $value){
-	$tile_id = $value["tile_id"];
-	$interest_id = $value["interest_id"];
+	$tile_id = $value;
+	$interest_id = getInterestFromTile($tile_id, $connection); 
 
 	//echo "tile_id is: ".$tile_id ." interest_id is: ".$interest_id." tile_placement is: ".$tile_placement;
 	updateMosaicWallTable($user_id, $interest_id , $tile_id , $tile_placement, $connection);
