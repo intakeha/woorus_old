267a73aa215b76d43f9070b90ca96aa1fe93bb29
Chonabot Intakeha <intakeha@gmail.com>
2011-06-06 04:28:49 +0000

    testing
    git-svn-id: https://woorus.com/svn/repos@709 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/showInbox.php
+++ actions/showInbox.php
@@ -33,7 +33,7 @@ $show_message_query = 	"SELECT message_text, sent_time, message_read, first_name
 					FROM `mail` 
 					LEFT JOIN `users` on users.id = mail.".$others."
 					WHERE mail.".$me."  =  '".$user_id."' AND message_deleted = 0 LIMIT ".$offset.", 5";
-
+die($show_message_query);
 $show_message_result = mysql_query($show_message_query, $connection) or die ("Error");
 
 //declare empy message array & set iterator to 1
