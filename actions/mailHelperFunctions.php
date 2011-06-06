368b6b05c0410940e4576eb92df0eb84efa8ad0b
Chonabot Intakeha <intakeha@gmail.com>
2011-06-06 03:46:17 +0000

    added date formatting
    git-svn-id: https://woorus.com/svn/repos@698 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

---
+++ actions/mailHelperFunctions.php
@@ -0,0 +1,39 @@
+<?php
+
+function convertTime($date_time){
+
+	$today = date("Y-m-d", time() );
+	$yesterday =  date("Y-m-d ", time()-86400) ;
+
+
+	if ($date_time > $today)
+	{
+		//show as time
+		$date = date_parse($date_time);
+		$hour =  $date['hour'];
+		$hour_modified = $hour % 12;
+		$minute = $date['minute'];
+		$am_or_pm = ($hour >= 12) ? "PM" : "AM";
+	
+		return $hour_modified.":".$minute." ".$am_or_pm;
+
+	}elseif ($date_time > $yesterday)
+	{
+		//show as yesterday
+		return "Yesterday";
+		
+	}else{
+
+		//show date
+		$date = date_parse($date_time);
+		$month =  $date['month'];
+		$day =  $date['day'];
+		$year =  $date['year'];
+		
+		return $month."-".$day."-".$year;
+		
+	}
+
+}
+
+?>
