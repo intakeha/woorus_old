9b1cdc5d10a8bc5a30ceb80111807d2ed08b353b
Chonabot Intakeha <intakeha@gmail.com>
2011-06-10 02:24:45 +0000

    testing out new date logic
    git-svn-id: https://woorus.com/svn/repos@750 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/readMessage.php
+++ actions/readMessage.php
@@ -46,7 +46,7 @@ if(mysql_num_rows($show_message_result) > 0){
 	$row = mysql_fetch_assoc($show_message_result)
 	$first_name =  $row['first_name'];
 	$message_text = $row['message_text'];
-	$sent_time = $row['sent_time'];
+	$sent_time = convertTime_LargeMessage($row['sent_time']);
 	$message_read = $row['message_read'];
 	$social_status = $row['social_status'];
 	$user_city_id = $row['user_city_id'];
