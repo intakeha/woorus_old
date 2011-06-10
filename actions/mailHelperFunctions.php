33f6a5514fbbf59c0de8108960d742580110592f
Chonabot Intakeha <intakeha@gmail.com>
2011-06-10 03:51:01 +0000

    updated date calls based on new php version
    git-svn-id: https://woorus.com/svn/repos@755 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/mailHelperFunctions.php
+++ actions/mailHelperFunctions.php
@@ -1,57 +1,38 @@
 <?php
 
-function convertTime($date_time){
+function convertTime($date_time_in){
 
+	$date_time = date_create($date_time_in);
+	
 	$today = date("Y-m-d", time() );
 	$yesterday =  date("Y-m-d ", time()-86400) ;
 
 	if ($date_time > $today)
 	{
-		//show as time
-		$date = date_parse($date_time);
-		$hour =  $date['hour'];
-		$hour_modified = $hour % 12;
-		$minute = $date['minute'];
-		$am_or_pm = ($hour >= 12) ? "PM" : "AM";
-	
-		return $hour_modified.":".$minute." ".$am_or_pm;
-
+		
+		$date =  date_format($date_time, 'g:i A');
+		
 	}elseif ($date_time > $yesterday)
 	{
 		//show as yesterday
-		return "Yesterday";
+		$date = "Yesterday";
 		
 	}else{
-
-		//show date
-		$date = date_parse($date_time);
-		$month =  $date['month'];
-		$day =  $date['day'];
-		$year =  $date['year'];
-		
-		return $month."-".$day."-".$year;
+	
+		$date = date_format($date_time, 'M-d');
 		
 	}
+	
+	return $date;
 
 }
 
-function convertTime_LargeMessage($date_time){
+function convertTime_LargeMessage($date_time_in){
 		
-		//show date
-		$date = date_parse($date_time);
-
-		$month =  $date['month'];
-		$day =  $date['day'];
-		$year =  $date['year'];
-		$hour = $date['hour'];
-		$minute = $date['minute'];
-		$second = $date['second'];
-		
-		$hour_modified = $hour % 12;
-		$am_or_pm = ($hour >= 12) ? "PM" : "AM";
-		
-		
-		return $month."-".$day."-".$year. " ".$hour_modified.":".$minute." ".$am_or_pm;
+	//show date
+	$date_time = date_create($date_time_in);
+	$date =  date_format($date_time, 'F j, Y g:i A');
+	return $date;
 
 }
 
