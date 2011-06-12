965ef8de218349374c6be964ca01b6cd5c49c97d
Chonabot Intakeha <intakeha@gmail.com>
2011-06-12 23:42:16 +0000

    updated block check to look both ways
    git-svn-id: https://woorus.com/svn/repos@787 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/contactHelperFunctions.php
+++ actions/contactHelperFunctions.php
@@ -23,7 +23,7 @@ function checkBlock($user_id, $other_user_id, $connection){
 
 	$check_block_query = "SELECT id
 					FROM `blocks`
-					WHERE blocks.user_blocker = '".$user_id."' AND blocks.user_blockee = '".$other_user_id."' AND blocks.active = 1";
+					WHERE (blocks.user_blocker = '".$user_id."' AND blocks.user_blockee = '".$other_user_id."' )  OR  (blocks.user_blockee = '".$user_id."' AND blocks.user_blocker = '".$other_user_id."' ) AND blocks.active = 1";
 
 	$check_block_result =  mysql_query($check_block_query, $connection) or die ("Error 12");
 	
