4c8d29a27fcbf0e868de45b9032d359cdac04c77
Chonabot Intakeha <intakeha@gmail.com>
2011-06-15 04:54:12 +0000

    added missed calls & new contact section
    git-svn-id: https://woorus.com/svn/repos@803 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/showFeed.php
+++ actions/showFeed.php
@@ -10,6 +10,9 @@ mysql_select_db($db_name);
 session_start();
 $user_id = $_SESSION['id'];
 
+$feed_array = array(); 
+
+//get missed calls
 $missed_calls_query = "SELECT conversations.update_time, users.first_name, users.social_status, users.block_status, users.user_city_id
 		FROM `conversations`
 		LEFT JOIN `users` on users.id =conversations.caller_id
@@ -17,8 +20,6 @@ $missed_calls_query = "SELECT conversations.update_time, users.first_name, users
 		LIMIT 0, 5";
 
 $missed_calls_result = mysql_query($missed_calls_query, $connection) or die ("Error");
-
-$missed_calls_array = array(); 
 
 $call_iterator = 1;
 while ($row = mysql_fetch_assoc($missed_calls_result)){
@@ -31,16 +32,47 @@ while ($row = mysql_fetch_assoc($missed_calls_result)){
 	$user_city_id = $row['user_city_id'];	
 	
 	//set data to send
-	$missed_calls_array[$call_iterator]['update_time']= $call_time;
-	$missed_calls_array[$call_iterator]['first_name']= $first_name;
-	$missed_calls_array[$call_iterator]['social_status']= $social_status;
-	$missed_calls_array[$call_iterator]['block_status']= $block_status;
-	$missed_calls_array[$call_iterator]['user_city_id']= $user_city_id;
+	$feed_array['missed_calls'][$call_iterator]['update_time']= $call_time;
+	$feed_array['missed_calls'][$call_iterator]['first_name']= $first_name;
+	$feed_array['missed_calls'][$call_iterator]['social_status']= $social_status;
+	$feed_array['missed_calls'][$call_iterator]['block_status']= $block_status;
+	$feed_array['missed_calls'][$call_iterator]['user_city_id']= $user_city_id;
 
 	$call_iterator++;
 }
 
-$output = json_encode($missed_calls_array);
+//get newly added contacts
+$new_contacts_query = "SELECT contacts.update_time, users.first_name, users.social_status, users.block_status, users.user_city_id
+		FROM `contacts`
+		LEFT JOIN `users` on users.id =contacts.user_contacter
+		WHERE contacts.user_contactee =  '".$user_id ."' AND contacts.update_time >  DATE_SUB(NOW(), INTERVAL 1 WEEK)
+		LIMIT 0, 5";
+
+$new_contacts_result = mysql_query($new_contacts_query, $connection) or die ("Error");
+
+$contacts_iterator = 1;
+while ($row = mysql_fetch_assoc($new_contacts_result)){
+
+	//retreive data
+	$call_time = convertTime($row['update_time']);
+	$first_name = $row['first_name'];
+	$social_status = $row['social_status'];	
+	$block_status = $row['block_status'];	
+	$user_city_id = $row['user_city_id'];	
+	
+	//set data to send
+	$feed_array['new_contacts'][$contacts_iterator]['update_time']= $call_time;
+	$feed_array['new_contacts'][$contacts_iterator]['first_name']= $first_name;
+	$feed_array['new_contacts'][$contacts_iterator]['social_status']= $social_status;
+	$feed_array['new_contacts'][$contacts_iterator]['block_status']= $block_status;
+	$feed_array['new_contacts'][$contacts_iterator]['user_city_id']= $user_city_id;
+
+	$contacts_iterator++;
+}
+
+
+
+$output = json_encode($feed_array);
 die($output);
 
 
