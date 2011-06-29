2683d7537086a8a2aee9cb0eccb03e0ad7e64d54
Chonabot Intakeha <intakeha@gmail.com>
2011-06-29 01:41:32 +0000

    checking to see if blocks are gone
    git-svn-id: https://woorus.com/svn/repos@838 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/userSearch.php
+++ actions/userSearch.php
@@ -31,11 +31,14 @@ mysql_select_db($db_name);
 //TO DO: need to look at online status!!!!!!!!!!!
 
 //look for interest based on ID & get users associated with the interest id retreived above
-$mosaic_query =  "SELECT interests.interest_name, mosaic_wall.user_id, users.first_name, users.social_status, users.block_status, mosaic_wall.interest_id, mosaic_wall.tile_id, tiles.tile_filename, tiles.user_id as tile_user_id, tiles.sponsored 
+$mosaic_query =  "SELECT interests.interest_name, mosaic_wall.user_id, users.first_name, users.social_status, users.block_status, mosaic_wall.interest_id, mosaic_wall.tile_id, tiles.tile_filename, tiles.user_id as tile_user_id, tiles.sponsored, BLOCKER.user_blocker, BLOCKER.user_blockee, BLOCKEE.user_blocker, BLOCKEE.user_blockee  
 			FROM `interests`, `mosaic_wall`
 			LEFT JOIN tiles ON mosaic_wall.tile_id = tiles.id
 			LEFT JOIN users ON users.id = mosaic_wall.user_id
+			LEFT OUTER JOIN blocks as BLOCKER on BLOCKER.user_blocker = mosaic_wall.user_id AND BLOCKER.user_blockee = '".$user_id."' 
+			LEFT OUTER JOIN blocks as BLOCKEE on BLOCKEE.user_blockee = mosaic_wall.user_id AND BLOCKEE.user_blocker = '".$user_id."' 
 			WHERE interests.interest_name =  '".mysql_real_escape_string($user_search)."' AND interests.id = mosaic_wall.interest_id AND mosaic_wall.user_id <> '".$user_id."' AND users.active_user = 1 
+			AND BLOCKEE.user_blockee IS NULL AND BLOCKEE.user_blockee IS NULL AND BLOCKER.user_blocker IS NULL AND BLOCKER.user_blockee IS NULL
 			GROUP BY mosaic_wall.user_id
 			LIMIT ".$offset.", 10";
 
@@ -45,7 +48,10 @@ $user_count_query = "SELECT COUNT(DISTINCT mosaic_wall.user_id)
 			FROM `interests`, `mosaic_wall`
 			LEFT JOIN tiles ON mosaic_wall.tile_id = tiles.id
 			LEFT JOIN users ON users.id = mosaic_wall.user_id
-			WHERE interests.interest_name =  '".mysql_real_escape_string($user_search)."' AND interests.id = mosaic_wall.interest_id AND mosaic_wall.user_id <> '".$user_id."' AND users.active_user = 1";
+			LEFT OUTER JOIN blocks as BLOCKER on BLOCKER.user_blocker = mosaic_wall.user_id AND BLOCKER.user_blockee = '".$user_id."' 
+			LEFT OUTER JOIN blocks as BLOCKEE on BLOCKEE.user_blockee = mosaic_wall.user_id AND BLOCKEE.user_blocker = '".$user_id."' 
+			WHERE interests.interest_name =  '".mysql_real_escape_string($user_search)."' AND interests.id = mosaic_wall.interest_id AND mosaic_wall.user_id <> '".$user_id."' 
+			AND users.active_user = 1 AND BLOCKEE.user_blockee IS NULL AND BLOCKEE.user_blockee IS NULL AND BLOCKER.user_blocker IS NULL AND BLOCKER.user_blockee IS NULL";
 
 //get count
 $user_count_query_result = mysql_query($user_count_query, $connection) or die ("Error 10");
@@ -65,49 +71,37 @@ if ( $user_count > 0) //we found matches
 		
 		//get data
 		$user_retreived_id = $row['user_id'];
-
-		$user_blocked = checkBlock($user_id, $user_retreived_id, $connection);
 		
-		if ($user_blocked == 0){
+		$interest_id = $row['interest_id'];
+		$tile_id = $row['tile_id'];
+		$interest_name = $row['interest_name'];
+		$tile_user_id = $row['tile_user_id'];
+		$tile_filename = $row['tile_filename'];
+		$sponsored = $row['sponsored'];
+		$first_name = $row['first_name'];
+		$social_status = $row['social_status'];
+		$block_status = $row['block_status'];
+		$tile_type = getTileType($sponsored, $tile_user_id, $user_id);
+		
+		$contact = checkContact($user_id, $user_retreived_id, $connection);
+		
+		//set data
+		$user_search_array[$user_iterator]['user_id'] = $user_retreived_id;
+		$user_search_array[$user_iterator]['first_name'] = $first_name;
+		$user_search_array[$user_iterator]['social_status'] = $social_status;
+		$user_search_array[$user_iterator]['block_status'] = $block_status;
+		$user_search_array[$user_iterator]['contact'] = $contact;
+		$user_search_array[$user_iterator]['tile_id'] = $tile_id;
+		$user_search_array[$user_iterator]['interest_id'] = $interest_id;
+		$user_search_array[$user_iterator]['tile_filename'] = $tile_filename;
+		$user_search_array[$user_iterator]['interest_name'] = $interest_name;
+		$user_search_array[$user_iterator]['tile_type'] = $tile_type;
 
-			//only show user if they arent blocked
-			$interest_id = $row['interest_id'];
-			$tile_id = $row['tile_id'];
-			$interest_name = $row['interest_name'];
-			$tile_user_id = $row['tile_user_id'];
-			$tile_filename = $row['tile_filename'];
-			$sponsored = $row['sponsored'];
-			$first_name = $row['first_name'];
-			$social_status = $row['social_status'];
-			$block_status = $row['block_status'];
-			$tile_type = getTileType($sponsored, $tile_user_id, $user_id);
-			
-			$contact = checkContact($user_id, $user_retreived_id, $connection);
-			
-			//set data
-			$user_search_array[$user_iterator]['user_id'] = $user_retreived_id;
-			$user_search_array[$user_iterator]['first_name'] = $first_name;
-			$user_search_array[$user_iterator]['social_status'] = $social_status;
-			$user_search_array[$user_iterator]['block_status'] = $block_status;
-			$user_search_array[$user_iterator]['contact'] = $contact;
-			$user_search_array[$user_iterator]['tile_id'] = $tile_id;
-			$user_search_array[$user_iterator]['interest_id'] = $interest_id;
-			$user_search_array[$user_iterator]['tile_filename'] = $tile_filename;
-			$user_search_array[$user_iterator]['interest_name'] = $interest_name;
-			$user_search_array[$user_iterator]['tile_type'] = $tile_type;
-
-			$user_iterator++;
-		}else{
-			//update count bc we have eliminated 1 user
-			$user_count--;
-			$user_search_array[0]['user_count'] = $user_count;
-		}
+		$user_iterator++;
+		
 	}
-}
-
-
-if ( $user_count == 0) //we found no matches OR no matches after block (not an elseif)
-{
+}else
+{  //we found no matches for users that are not blocked
 	$error_message = "We found no matches for your interest. Please search by a new interest or meet someone in the lounge.";
 	sendToJS(0, $error_message);
 }
