a9502f043ba5aab47891f463b1f686f1ccc9f3e7
Chonabot Intakeha <intakeha@gmail.com>
2011-06-29 03:52:23 +0000

    updated to optimize
    git-svn-id: https://woorus.com/svn/repos@850 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/blockUser.php
+++ actions/blockUser.php
@@ -15,24 +15,18 @@ $block_reason = "They were rude!";
 $connection = mysql_connect($db_host, $db_user, $db_pass) or die;
 mysql_select_db($db_name);
 
-$query_block_user_search = "SELECT id FROM `blocks` WHERE user_blockee = '".mysql_real_escape_string($user_id_blockee)."' AND user_blocker = '".mysql_real_escape_string($user_id_blocker)."' ";
-$result = mysql_query($query_block_user_search, $connection) or die ("Error");
 
-if (mysql_num_rows($result) == 1)
-{
-//the person has blocked the user before--and has unblocked (or just found again somehow...)
-	$row = mysql_fetch_assoc($result);
-	$block_id = $row['id']; 
-	$update_query = "UPDATE `blocks` SET active = 1, update_time = NOW() , block_reason = '".mysql_real_escape_string($block_reason)."' 
-				WHERE id =  '".$block_id."' ";
-	$result = mysql_query($update_query, $connection) or die ("Error");
-}
-else{
-//first time user has blocked the other
+$update_query = "UPDATE `blocks` SET active = 1, update_time = NOW() , block_reason = '".mysql_real_escape_string($block_reason)."' 
+				WHERE id =  user_blockee = '".mysql_real_escape_string($user_id_blockee)."' AND user_blocker = '".mysql_real_escape_string($user_id_blocker)."' ";
+$result = mysql_query($update_query, $connection) or die ("Error");
+
+if (mysql_affected_rows($result)==0) {
+
 	$query_block_user = "INSERT INTO `blocks` (id, user_blockee, user_blocker, update_time, block_reason, active) VALUES
-							(NULL, '".mysql_real_escape_string($user_id_blockee)."' , '".mysql_real_escape_string($user_id_blocker)."' ,NOW(), '".mysql_real_escape_string($block_reason)."' ,1) ";
+								(NULL, '".mysql_real_escape_string($user_id_blockee)."' , '".mysql_real_escape_string($user_id_blocker)."' ,NOW(), '".mysql_real_escape_string($block_reason)."' ,1) ";
 	$result = mysql_query($query_block_user, $connection) or die ("Error");
 }
+
 
 //then, make sure the blockee is not on the blocker's contacts & also remove the blocker from the blockee's contacts
 $update_contacts_query = "UPDATE `contacts` SET active = 0, update_time = NOW() 
