3e6cd807563cfdaaf595278443b8f9d37ee40954
Chonabot Intakeha <intakeha@gmail.com>
2011-06-29 04:04:42 +0000

    fixed syntax
    git-svn-id: https://woorus.com/svn/repos@852 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/blockUser.php
+++ actions/blockUser.php
@@ -17,9 +17,8 @@ mysql_select_db($db_name);
 
 
 $update_query = "UPDATE `blocks` SET active = 1, update_time = NOW() , block_reason = '".mysql_real_escape_string($block_reason)."' 
-				WHERE id =  user_blockee = '".mysql_real_escape_string($user_id_blockee)."' AND user_blocker = '".mysql_real_escape_string($user_id_blocker)."' ";
+				WHERE user_blockee = '".mysql_real_escape_string($user_id_blockee)."' AND user_blocker = '".mysql_real_escape_string($user_id_blocker)."' ";
 $result = mysql_query($update_query, $connection) or die ("Error");
-
 
 if (mysql_affected_rows() == 0) {
 
