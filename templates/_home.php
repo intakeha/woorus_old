9b5f7f7997c8c289e0ee79d1e072074b09641815
Chonabot Intakeha <intakeha@gmail.com>
2011-07-18 02:08:06 +0000

    Updated 3:2 ratio on the home page
    git-svn-id: https://woorus.com/svn/repos@865 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- templates/_home.php
+++ templates/_home.php
@@ -2,7 +2,7 @@
 	<div id="profile">
         <div id="profile_pic"><img src="images/global/user_pic.png"></div>
 		<div id="profile_frame"><a href="#" title="Jessica"></a></div>
-        <div id="profile_name"><div id="online_status"></div><span>Jessica</span><br />Palo Alto, CA | United States</div>
+        <div id="profile_name"><div id="online_status" class="float_right"></div><span>Jessica</span><br />Palo Alto, CA | United States</div>
 		<a id="announcement" href="canvas.php?page=mosaic">
 				<div><span>Mosaic Wall</span><br>
 				Customize your mosaic wall and show the world who you are! Use personalized tiles to build your interests 
@@ -14,111 +14,8 @@
 		<div id="warning"></div>
     </div>
    	<div class="pagination_home"><a class="arrows pagination_left" href="#"></a></div>
-   	<div id="feeds">
-    	<ul>
-        	<li class="feed_entry">
-            	<div class="feed_content">
-                	<a class="feed_profile" href="#"><img src="images/users/james.png" /></a>
-                    <div class="feed_display">
-                    	<div class="feed_update">
-	                    	<div class="feed_message">You've been added to James' contact list.<br /><font>February 15 at 7:32pm</font></div>
-                            <div class="feed_online_status"></div>
-                            <div class="feed_social_status"></div>
-                        </div>
-                        <div class="feed_links">
-                            <div class="feed_actions">
-                                <a class="add_button_sm" href="#"></a>
-                                <a class="write_button_sm" href="#"></a>
-                                <a class="talk_button_sm" href="#"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="archive_button" href="#"></a>
-                </div>
-            </li>
-            <li class="feed_entry">
-            	<div class="feed_content">
-                	<a class="feed_profile" href="#"><img src="images/users/amy.png" /></a>
-                    <div class="feed_display">
-                    	<div class="feed_update">
-	                    	<div class="feed_message">Amy likes your custom <a href="#">Starwood</a> tile.<br /><font>February 14 at 2:12pm</font></div>
-                            <div class="feed_online_status"></div>
-                            <div class="feed_social_status"></div>
-                        </div>
-                        <div class="feed_links">
-                        	<a class="feed_interest" href="#"><img src="images/interests/starwood.png" /></a>
-                            <div class="feed_actions">
-                                <a class="add_button_sm" href="#"></a>
-                                <a class="write_button_sm" href="#"></a>
-                                <a class="talk_button_sm" href="#"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="archive_button" href="#"></a>
-                </div>
-            </li>
-            <li class="feed_entry">
-            	<div class="feed_content">
-                	<a class="feed_profile" href="#"><img src="images/users/john.png" /></a>
-                    <div class="feed_display">
-                    	<div class="feed_update">
-	                    	<div class="feed_message">John also likes <a href="#">The Killers</a>.<br /><font>February 14 at 10:14am</font></div>
-                            <div class="feed_online_status"></div>
-                            <div class="feed_social_status"></div>
-                        </div>
-                        <div class="feed_links">
-                        	<a class="feed_interest" href="#"><img src="images/interests/killers.png" /></a>
-                            <div class="feed_actions">
-                                <a class="add_button_sm" href="#"></a>
-                                <a class="write_button_sm" href="#"></a>
-                                <a class="talk_button_sm" href="#"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="archive_button" href="#"></a>
-                </div>
-            </li>
-            <li class="feed_entry">
-            	<div class="feed_content">
-                	<a class="feed_profile" href="#"><img src="images/users/candice.png" /></a>
-                    <div class="feed_display">
-                    	<div class="feed_update">
-	                    	<div class="feed_message">Candice added <a href="#">fashion</a> as an interest.<br /><font>February 14 at 9:44am</font></div>
-                            <div class="feed_online_status"></div>
-                            <div class="feed_social_status"></div>
-                        </div>
-                        <div class="feed_links">
-                        	<a class="feed_interest" href="#"><img src="images/interests/fashion.png" /></a>
-                            <div class="feed_actions">
-                                <a class="write_button_sm" href="#"></a>
-                                <a class="talk_button_sm" href="#"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="archive_button" href="#"></a>
-                </div>
-            </li>
-            <li id="feed_last_entry" class="feed_entry">
-            	<div class="feed_content">
-                	<a class="feed_profile" href="#"><img src="images/users/sanjay.png" /></a>
-                    <div class="feed_display">
-                    	<div class="feed_update">
-	                    	<div class="feed_message">You missed a call from Sanjay.<br /><font>February 13 at 12:21am</font></div>
-                            <div class="feed_online_status"></div>
-                            <div class="feed_social_status"></div>
-                        </div>
-                        <div class="feed_links">
-                            <div class="feed_actions">
-                                <a class="block_button_sm" href="#"></a>
-                                <a class="add_button_sm" href="#"></a>
-                                <a class="talk_button_sm" href="#"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="archive_button" href="#"></a>
-                </div>
-            </li>
-        </ul>
+   	<div id="updates">
+
 
     </div>
    	<div class="pagination_home"><a class="arrows pagination_right" href="#"></a></div>
