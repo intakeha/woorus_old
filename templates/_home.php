e2f9c23880bdecbd72db87bd9a967b59366a8857
Chonabot Intakeha <intakeha@gmail.com>
2011-07-22 05:50:59 +0000

    Added upload profile picture
    git-svn-id: https://woorus.com/svn/repos@889 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- templates/_home.php
+++ templates/_home.php
@@ -55,6 +55,17 @@
 		</div>
     </div>
    	<div id="updates_right" class="pagination_home" style="display: none;"><a class="arrows pagination_right" href="#"></a></div>
+    <div id="upload_profile_pic" style="display: none;">
+    	<p>Select a photo for your profile picture:</p>
+		<form id="profile_upload_form" action="actions/uploadProfilePicture.php" method="post" enctype="multipart/form-data">
+            <input class="text_form" type="file" name="file" id="file" style="width: 430px;" /><br />
+            <input class="buttons" id="profile_pic_upload" type="submit" name="filename" value="Upload">
+            <div class="error_text" id="profile_upload_error"></div>
+            <div class="success_text" id="profile_upload_success" style="display: none;"></div>
+            <img id="tile_loading" style="display: none;" src="images/global/loading.gif" />
+		</form>
+        <div id="profile_frame"></div><div id="profile_pic"><img src="images/global/user_pic.png"></div>
+    </div>
 </div>
 
 <script type="text/javascript">
@@ -68,6 +79,7 @@
 	
 	$('#profile_frame').click(function(){
 		hideProfile();
+		$('#upload_profile_pic').show();
 	});
 	
 	function hideProfile(){
@@ -75,4 +87,40 @@
 		$('#profile_social_status').hide();
 		$('#updates').hide();
 	}
+	
+	// Upload picture file for tile crop
+	$('#tile_pic_upload').click(function(){
+		$("#tile_loading")
+		.ajaxStart(function(){
+			$('#tile_upload_error').hide();
+			$('#tile_upload_success').hide();
+			$(this).show();
+		})
+		.ajaxComplete(function(){
+			$(this).hide();
+		});
+		
+		$.ajaxFileUpload({
+			url: 'actions/upload_file.php',
+			secureuri: false,
+            fileElementId:'file',
+            dataType: 'json',
+			success: function(data){
+				if (data.success == 0){
+					$('#tile_upload_success').hide();
+					$('#tile_upload_error').show();
+					$('#tile_upload_error').html(data.message); 
+				} else {
+					$('.pagination_mosaic').hide();
+					$('#tiles').hide();
+					$('#tile_crop').show();
+					$('input[name=assign_tag]').val('');
+					$('.tile_pic').attr('src','images/temporary/'+data.message);
+					$('input[name=cropFile]').val(data.message);
+				}
+			}
+		})
+		
+		return false;
+	});
 </script>
