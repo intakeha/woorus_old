6f3fb6654d03aabca76b3b56ccc0a44ed6e88117
Chonabot Intakeha <intakeha@gmail.com>
2011-07-31 21:23:11 +0000

    updated to bring in new info, as per our new layout
    git-svn-id: https://woorus.com/svn/repos@914 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/showFeed.php
+++ actions/showFeed.php
@@ -13,10 +13,11 @@ $user_id = $_SESSION['id'];
 $feed_array = array(); 
 
 //get missed calls
-$missed_calls_query = "SELECT conversations.update_time, users.first_name, users.social_status, users.block_status, users.user_city_id
+$missed_calls_query = "SELECT conversations.caller_id, conversations.update_time, users.first_name, profile_picture.profile_filename_small
 		FROM `conversations`
 		LEFT JOIN `users` on users.id =conversations.caller_id
-		WHERE conversations.caller_id =  '".$user_id ."' AND conversations.update_time >  DATE_SUB(NOW(), INTERVAL 1 WEEK)
+		LEFT OUTER JOIN `profile_picture` on profile_picture.user_id = conversations.caller_id
+		WHERE conversations.callee_id =  '".$user_id ."' AND conversations.call_accepted = 'missed' AND conversations.update_time >  DATE_SUB(NOW(), INTERVAL 1 WEEK) 
 		LIMIT 0, 5";
 
 $missed_calls_result = mysql_query($missed_calls_query, $connection) or die ("Error");
@@ -25,19 +26,19 @@ $call_iterator = 1;
 while ($row = mysql_fetch_assoc($missed_calls_result)){
 
 	//set data to send
-	$feed_array['missed_calls'][$call_iterator]['update_time']= convertTime($row['update_time']);
+	//$feed_array['missed_calls'][$call_iterator]['update_time']= convertTime($row['update_time']);
 	$feed_array['missed_calls'][$call_iterator]['first_name']= $row['first_name'];
-	$feed_array['missed_calls'][$call_iterator]['social_status']= $row['social_status'];
-	$feed_array['missed_calls'][$call_iterator]['block_status']= $row['block_status'];
-	$feed_array['missed_calls'][$call_iterator]['user_city_id']= $row['user_city_id'];
+	$feed_array['missed_calls'][$call_iterator]['user_id']= $row['caller_id'];
+	$feed_array['missed_calls'][$call_iterator]['profile_filename_small']= $row['profile_filename_small'];
 
 	$call_iterator++;
 }
 
 //get newly added contacts
-$new_contacts_query = "SELECT contacts.update_time, users.first_name, users.social_status, users.block_status, users.user_city_id
+$new_contacts_query = "SELECT contacts.user_contacter, contacts.update_time, users.first_name, profile_picture.profile_filename_small,
 		FROM `contacts`
-		LEFT JOIN `users` on users.id =contacts.user_contacter
+		LEFT JOIN `users` on users.id = contacts.user_contacter
+		LEFT OUTER JOIN `profile_picture` on profile_picture.user_id = contacts.user_contacter
 		WHERE contacts.user_contactee =  '".$user_id ."' AND contacts.update_time >  DATE_SUB(NOW(), INTERVAL 1 WEEK)
 		LIMIT 0, 5";
 
@@ -46,20 +47,14 @@ $new_contacts_result = mysql_query($new_contacts_query, $connection) or die ("Er
 $contacts_iterator = 1;
 while ($row = mysql_fetch_assoc($new_contacts_result)){
 
-	//retreive data
-	$call_time = convertTime($row['update_time']);
-	$first_name = $row['first_name'];
-	$social_status = $row['social_status'];	
-	$block_status = $row['block_status'];	
-	$user_city_id = $row['user_city_id'];	
 	
 	//set data to send
-	$feed_array['new_contacts'][$contacts_iterator]['update_time']= $call_time;
-	$feed_array['new_contacts'][$contacts_iterator]['first_name']= $first_name;
-	$feed_array['new_contacts'][$contacts_iterator]['social_status']= $social_status;
-	$feed_array['new_contacts'][$contacts_iterator]['block_status']= $block_status;
-	$feed_array['new_contacts'][$contacts_iterator]['user_city_id']= $user_city_id;
-
+	//$feed_array['new_contacts'][$contacts_iterator]['update_time']= convertTime($row['update_time']);
+	$feed_array['new_contacts'][$contacts_iterator]['first_name']= $row['first_name'];
+	$feed_array['new_contacts'][$contacts_iterator]['user_id']= $row['user_contacter'];
+	$feed_array['new_contacts'][$contacts_iterator]['profile_filename_small']= $row['profile_filename_small'];
+	
+	
 	$contacts_iterator++;
 }
 
@@ -89,6 +84,8 @@ while ($row = mysql_fetch_assoc($new_interests_result)){
 }
 
 //get users who have shared interests
+
+//select the random interest we're going to use
 $get_common_interest = "SELECT mosaic_wall.interest_id
 					FROM `mosaic_wall`
 					WHERE mosaic_wall.tile_id <> 0 AND mosaic_wall.interest_id <> 0 AND mosaic_wall.user_id = '".$user_id."' 
@@ -101,9 +98,10 @@ if (mysql_num_rows($common_interest_id) > 0){
 	$interest_id = $row['interest_id'];
 }
 
-$common_interests_query = "SELECT users.id as user_id
+$common_interests_query = "SELECT users.id as user_id, users.first_name, profile_picture.profile_filename_small
 				FROM `mosaic_wall`
 				LEFT JOIN users ON users.id = mosaic_wall.user_id
+				LEFT OUTER JOIN `profile_picture` on profile_picture.user_id = mosaic_wall.user_id
 				WHERE mosaic_wall.interest_id = '".$interest_id ."' AND mosaic_wall.user_id <> '".$user_id."' AND mosaic_wall.update_time >  DATE_SUB(NOW(), INTERVAL 1 MONTH)
 				GROUP BY users.id
 				ORDER BY RAND()
@@ -115,7 +113,9 @@ $common_interests_iterator = 1;
 while ($row = mysql_fetch_assoc($common_interests_result)){
 
 	//retreive data
+	$feed_array['common_interests'][$common_interests_iterator]['first_name']= $row['first_name'];
 	$feed_array['common_interests'][$common_interests_iterator]['user_id']= $row['user_id'];
+	$feed_array['common_interests'][$common_interests_iterator]['profile_filename_small']= $row['profile_filename_small'];
 
 	$common_interests_iterator++;
 }
