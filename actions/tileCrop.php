86338c255af770f790ab1d6c42bc2b4eee5789f6
Chonabot Intakeha <intakeha@gmail.com>
2011-11-02 02:40:56 +0000

    name change for clarity
    git-svn-id: https://woorus.com/svn/repos@984 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

---
+++ actions/tileCrop.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+crop.php
+
+Scipt used when a user is creating their own tile from an uploaded picture. The inputs are the coordinates of the
+crop, as well as the width & height, the name of the input file, and the name of the new tile
+
+It creates the thumbnail image and then stores it as a user interest and on the user's mosaic wall.
+*/
+
+require_once('imageFunctions.php');
+require_once('connect.php');
+require_once('validations.php');
+require_once('mosaicWallHelperFunctions.php');
+
+$thumb_width = "75";		// Width of thumbnail image
+$thumb_height = "75";		// Height of thumbnail image
+
+session_start();
+$user_id = $_SESSION['id'];
+
+$x1 = validateCoordinates(strip_tags($_POST["x1"]));
+$y1 = validateCoordinates(strip_tags($_POST["y1"]));
+$x2 = validateCoordinates(strip_tags($_POST["x2"]));
+$y2 = validateCoordinates(strip_tags($_POST["y2"]));
+$w = validateCoordinates(strip_tags($_POST["w"]));
+$h = validateCoordinates(strip_tags($_POST["h"]));
+$picture_name_input = strip_tags($_POST["cropFile"]); 
+$tile_name = validateInterestTag(strip_tags($_POST["assign_tag"]));
+
+//if coordinates are null, send error message to JS
+if ($x1 == NULL || $y1 == NULL || $x2 == NULL || $y2 == NULL || $w == NULL || $h == NULL || $picture_name_input == NULL || $tile_name == NULL){
+
+	$message = "Please click on the image & crop to create your tile."; 
+	sendToJS(0, $message);
+}
+
+//get ext & re-name
+$file_ext = strtolower(substr($picture_name_input, strrpos($picture_name_input, '.') + 1));  //one day, this will always be .jpg!
+$key = strtotime(date('Y-m-d H:i:s'));
+
+$picture_name = $user_id . "_" . $key . "." . $file_ext;// name the image w/ random number; should be of form: UID_#####.***
+
+//set file path based on filename
+$large_path = "../images/temporary";
+$thumbnail_path = "../images/interests";
+
+$large_image_location = $large_path."/".$picture_name_input;
+$thumb_image_location = $thumbnail_path."/".$picture_name;
+
+//Scale the image to the thumbnail size & save
+$scale = $thumb_width/$w;
+$cropped = resizeThumbnailImage($thumb_image_location, $large_image_location,$w,$h,$x1,$y1,$scale);
+
+//delete the temp file
+unlink($large_image_location);
+
+
+//---------------Now, enter the image /tile / interests into the DB-------
+
+//connect
+$connection = mysql_connect($db_host, $db_user, $db_pass) or die;
+mysql_select_db($db_name);
+
+//validate tag (alphanumeric)!!
+
+//see if interest exists: if already in the db--> get id, if new-> add line, get id.
+$interest_id = lookupInterestID($tile_name, $connection);
+
+if ($interest_id == NULL)
+{
+	$query_add_interest = "INSERT INTO `interests` (id, interest_name, category, facebook_id, facebook_category, update_time, user_id) VALUES
+							(NULL, '".mysql_real_escape_string($tile_name)."' , NULL, NULL,  NULL, NOW(), '".mysql_real_escape_string($user_id)."') ";
+	$result = mysql_query($query_add_interest, $connection) or die ("Error 5");
+	$interest_id = lookupInterestID($tile_name, $connection); //now lookup should find something
+}
+
+//save the tile & get tile id
+updateTileTable($user_id, $interest_id, 0, $picture_name, $connection); 
+$tile_id = lookupTileID($picture_name, $connection);
+
+//update user interests table (requires interest id & tile id)
+updateUserInterestTable($user_id, $interest_id, $tile_id, $connection); //add this as an interest of the user, its *new* for them
+
+//lookup tile placment
+$tile_placement = getTilePlacement($user_id, $connection);
+
+if ($tile_placement == NULL)
+{
+	//send jSON array with message that the wall is full
+	$success_message = "Your wall is full. Your tile has been placed in the tile bank.";
+
+}else
+{
+	//add to user's mosaic wall
+	updateMosaicWallTable($user_id, $interest_id, $tile_id, $tile_placement, $connection);
+
+	//send jSON array with success flag, message, filename
+	$success_message = "Your new tile has been added to your wall.";
+
+}
+
+$messageToSend = array('success' => 1, 'message'=>$success_message, 'tile_filename'=>$picture_name, 'tile_id'=>$tile_id, 'interest_name'=>$tile_name, 'tile_type'=>'U', 'interest_id'=>$interest_id, 'tile_placement'=>$tile_placement);
+$output = json_encode($messageToSend);
+die($output);
+
+?>
---
+++ actions/uploadTile.php
@@ -0,0 +1,127 @@
+<?php
+/*
+uploadFile.php
+
+Used when the user is uploading a new image for use as a tile. This takes the image, checks for acceptable format & resizes if too big.
+Need to change the name
+*/
+require_once('imageFunctions.php');
+require_once('validations.php');
+
+session_start();
+$user_id = $_SESSION['id'];
+
+$max_dimension = "400";		// Max width allowed for the large image
+$min_dimension = "75";
+
+/*image type:
+1 = GIF, 2 = JPG, 3 = PNG, 4 = SWF, 5 = PSD, 6 = BMP, 7 = TIFF(orden de bytes intel), 8 = TIFF(orden de bytes motorola), 9 = JPC, 10 = JP2, 11 = JPX, 12 = JB2, 13 = SWC, 14 = IFF, 15 = WBMP, 16 = XBM.
+*/
+
+//acceptable image types
+$gif = 1;
+$jpg = 2;
+$png = 3;
+$bmp = 6;
+
+//check image type
+$validImageType = array($gif, $jpg, $png, $bmp);
+list($width, $height, $type, $attr) = getimagesize($_FILES["file"]["tmp_name"]); 
+
+//check extension
+$validExtensions = array("png", "gif", "jpeg", "jpg", "bmp");
+$ext = end(explode(".",$_FILES["file"]["name"]));
+$ext2= strtolower($ext);
+
+//check image type 2 ways, extensions & size
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/bmp")
+|| ($_FILES["file"]["type"] == "image/png")
+|| ($_FILES["file"]["type"] == "image/x-png"))
+&& in_array($ext2,$validExtensions)
+&& in_array($type,$validImageType))
+{
+	//check image size
+	if($_FILES["file"]["size"] > 1000000){
+		$error_message = "Please select a smaller image.";
+		sendToJS(0, $error_message);
+	}
+	
+	//check for error code
+	if ($_FILES["file"]["error"] > 0)
+	{
+		$error_message = "Return Code: " . $_FILES["file"]["error"];
+		sendToJS(0, $error_message);
+	}
+	else
+	{
+		//rename image & add back on extension
+		$file_name_orig = basename($_FILES['file']['name']);
+		$file_ext = strtolower(substr($file_name_orig, strrpos($file_name_orig, '.') + 1));
+		
+		$key = strtotime(date('Y-m-d H:i:s'));
+		$file_name = $user_id . "_temp_" . $key . "." . $file_ext;// name the image w/ random number; should be of form: UID_temp_#####.***
+		
+		$large_path = "../images/temporary";
+		
+		$large_image_location = $large_path."/".$file_name;
+		
+		//save large image
+		move_uploaded_file($_FILES["file"]["tmp_name"],
+		$large_image_location);      
+		chmod($large_image_location, 0777);
+		
+		//get height, width & scale if too big
+		$width = getWidth($large_image_location);
+		$height = getHeight($large_image_location);			
+
+		//find out which dimension is larger (we need both min and max)
+		//this only works because the picture is a square
+		if ($width > $height){
+			$max_dimension_num = $width;
+			$min_dimension_num = $height;
+		}else
+		{
+			$max_dimension_num = $height;
+			$min_dimension_num = $width;
+		}
+		
+		//error if image is too small
+		if ($min_dimension_num < $min_dimension){
+			$error_message = "Please use a larger image.";
+			sendToJS(0, $error_message);
+		}
+	
+		/*
+		//test file conversion to .jpg
+		$large_image_location = convertImage($large_image_location);
+		chmod($large_image_location, 0777);*/
+		
+		//Scale the image if it is greater than the max dimension
+		if ($max_dimension_num > $max_dimension){
+			$scale = $max_dimension/$max_dimension_num;
+			$uploaded = resizeImage($large_image_location,$width,$height,$scale);
+		}else{
+			$scale = 1;
+			$uploaded = resizeImage($large_image_location,$width,$height,$scale);
+		} 
+		
+		//set data array of picture location & print to JavaSrcipt
+		
+		$new_file_name  = substr($file_name, 0, strrpos($file_name, '.')).".jpg";
+		sendToJS(1, $new_file_name);
+
+		//sendToJS(1, $file_name);
+		
+	}
+}
+else
+{
+	$error_message = "Please select a valid file type.";
+	sendToJS(0, $error_message);
+}
+
+?>
