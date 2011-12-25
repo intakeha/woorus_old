a54c59c0873c0c7b35e9452dbf8a5f2946007128
Chonabot Intakeha <intakeha@gmail.com>
2011-12-25 03:46:28 +0000

    Optimized cityList search for performance.
    git-svn-id: https://woorus.com/svn/repos@1014 5aa394f6-f131-4bb5-9076-cee5fef3a3f9

--- actions/cityList.php
+++ actions/cityList.php
@@ -9,30 +9,29 @@ This is for auto-populate of cities
 require_once('connect.php');
 require_once('validations.php');
 
-$q =  strtolower(validateCity_Search(strip_tags($_GET["q"])));
+$q =  strtolower(strip_tags($_GET["q"]));
 
 //connect
 $connection = mysql_connect($db_host, $db_user, $db_pass) or die;
 mysql_select_db($db_name);
 
-
 $city_query = "SELECT city.id, city.city_name
 		FROM `city`
-		ORDER by city.city_name ASC";
+		WHERE city.city_name LIKE '".mysql_real_escape_string($q)."%' LIMIT 5";
 
 $city_result = mysql_query($city_query, $connection) or die ("Error");
 
 $city_array = array();
-while ($row = mysql_fetch_assoc($city_result)){
+$city_iterator = 1;
 
+while ($row = mysql_fetch_assoc($city_result)){
 	//retreive ID & city name
 	$city_id = $row['id'];
 	$city_name = $row['city_name'];	
 	
 	//set city ID & city name pairs
 	$city_array[$city_id] = $city_name;
-}
-
+};
 
 $result = array();
 $search_iterator = 1;
@@ -51,6 +50,5 @@ foreach ($city_array as $key=>$value) {
 
 $output = json_encode($result);
 die($output);
-
 
 ?>
